04e15314d853e00521c4b3fff6dccb803a6872d1
Tomas Kocak <tomas.kocak@gmail.com>
2020-10-25 13:52:19 +0100

    translated commet

--- src/components/ApiTest.tsx
+++ src/components/ApiTest.tsx
@@ -4,8 +4,9 @@ import axios from "axios";
 
 export const ApiTest: React.FC = () => {
   useEffect(() => {
-    // Any /api/* path is redirected to localhost:8000/* where the api is running
-    // /api/cms/menu-item/on_site/1/ return a json with all the menu items for Strom sub-page
+    // Všetky cesty typu "/api/*" sú presmerované na "localhost:8000/*" kde beží náš django server
+    // spustený pomocou python manage.py runserver. Api z cesty je odstránené.
+    // "/api/cms/menu-item/on_site/1/" z príkladu nižšie načíta položky menu pre podstránku Stromu
     axios.get("/api/cms/menu-item/on_site/1/").then((res) => {
       console.log(res.data);
     });
