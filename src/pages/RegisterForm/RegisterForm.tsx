75135ab49d9fa4a8f7748d2b64b28cdb33e7ccd7
matushl <hlavacik.matus@gmail.com>
2020-11-30 20:48:09 +0100

    add component with register form

---
+++ src/pages/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,11 @@
+import './RegisterForm.css'
+
+import React from 'react'
+
+export const RegisterForm: React.FC = () => {
+  return (
+    <div className="registerform">
+      <h1>Registrácia</h1>
+    </div>
+  )
+}
