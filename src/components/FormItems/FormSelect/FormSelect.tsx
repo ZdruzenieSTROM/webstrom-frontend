b001dd00e88a3e889447448a7307c4a2700a7c66
matushl <hlavacik.matus@gmail.com>
2021-01-19 18:14:01 +0100

    fix eslint errors

--- src/components/FormItems/FormSelect/FormSelect.tsx
+++ src/components/FormItems/FormSelect/FormSelect.tsx
@@ -4,29 +4,29 @@ import {Controller} from 'react-hook-form'
 
 const MuiSelect = (props: any) => {
   const {label, name, options} = props
-
   return (
     <FormControl fullWidth={true}>
       <InputLabel htmlFor={name}>{label}</InputLabel>
       <Select id={name} {...props}>
-        {options.map((item: {id: string; label: string}) => (
-          <MenuItem key={item.id} value={item.id}>
-            {item.label}
-          </MenuItem>
-        ))}
+        {options &&
+          options.map((item: {id: string; label: string}) => (
+            <MenuItem key={item.id} value={item.id}>
+              {item.label}
+            </MenuItem>
+          ))}
       </Select>
     </FormControl>
   )
 }
 
-function FormSelect(props: any) {
+const FormSelect = (props: any) => {
   const {control, name, label, options} = props
   return (
     <React.Fragment>
       <Controller
         name={name}
         control={control}
-        render={(props) => <MuiSelect options={options} label={label} {...props} />}
+        render={({ref, ...props}) => <MuiSelect options={options} label={label} {...props} />}
       />
     </React.Fragment>
   )
