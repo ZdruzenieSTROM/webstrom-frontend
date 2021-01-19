5bbb9d1d9966853b50df7034b4d06e6e3171d82f
matushl <hlavacik.matus@gmail.com>
2021-01-19 18:14:58 +0100

    add working loading data to selectFiels

--- src/components/FormItems/FormSelect/FormSelect.tsx
+++ src/components/FormItems/FormSelect/FormSelect.tsx
@@ -1,35 +1,27 @@
-import {FormControl, InputLabel, MenuItem, Select} from '@material-ui/core'
-import React from 'react'
-import {Controller} from 'react-hook-form'
+import {FormControl, InputLabel, MenuItem, Select, SelectProps} from '@material-ui/core'
+import React, {FC} from 'react'
+import {Control, Controller} from 'react-hook-form'
 
-const MuiSelect = (props: any) => {
-  const {label, name, options} = props
+export type SelectItem = {id: number; label: string}
+
+export const FormSelect: FC<SelectProps & {name: string; control: Control; options: SelectItem[]}> = (props) => {
+  const {label, name, options, control} = props
   return (
     <FormControl fullWidth={true}>
       <InputLabel htmlFor={name}>{label}</InputLabel>
-      <Select id={name} {...props}>
-        {options &&
-          options.map((item: {id: string; label: string}) => (
-            <MenuItem key={item.id} value={item.id}>
-              {item.label}
-            </MenuItem>
-          ))}
-      </Select>
-    </FormControl>
-  )
-}
-
-const FormSelect = (props: any) => {
-  const {control, name, label, options} = props
-  return (
-    <React.Fragment>
       <Controller
         name={name}
         control={control}
-        render={({ref, ...props}) => <MuiSelect options={options} label={label} {...props} />}
+        as={
+          <Select id={name} {...props}>
+            {options.map(({id, label}) => (
+              <MenuItem key={id} value={id}>
+                {label}
+              </MenuItem>
+            ))}
+          </Select>
+        }
       />
-    </React.Fragment>
+    </FormControl>
   )
 }
-
-export default FormSelect
--- src/pages/RegisterForm/RegisterForm.tsx
+++ src/pages/RegisterForm/RegisterForm.tsx
@@ -1,20 +1,31 @@
 import './RegisterForm.css'
 
 import {Button} from '@material-ui/core'
-import React from 'react'
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
 import {useForm} from 'react-hook-form'
 
 import FormCheckbox from '../../components/FormItems/FormCheckbox/FromCheckbox'
 import FormInput from '../../components/FormItems/FormInput/FormInput'
-import FormSelect from '../../components/FormItems/FormSelect/FormSelect'
+import {FormSelect, SelectItem} from '../../components/FormItems/FormSelect/FormSelect'
 
 const defaultValues = {
+  school_not: false,
+  county: '',
+  district: '',
+  school_name: '',
+  school: '',
+  school_not_found: false,
   year_of_graduation: '',
   gdpr: false,
 }
 
 const RegisterForm: React.FC = () => {
-  const {handleSubmit, control} = useForm({defaultValues})
+  const {handleSubmit, control, watch} = useForm({defaultValues})
+  const fields = watch(Object.keys(defaultValues))
+  const [schoolsItems, setSchoolItems] = useState<SelectItem[]>([])
+  const [districtItems, setDistrictItems] = useState<SelectItem[]>([])
+  const [countyItems, setCountyItems] = useState<SelectItem[]>([])
 
   const grades = [
     {
@@ -103,6 +114,36 @@ const RegisterForm: React.FC = () => {
     },
   ].map((grade) => ({id: grade.pk, label: grade.fields.name}))
 
+  useEffect(() => {
+    const fetchCounties = async () => {
+      const schools = await axios.get(`/api/personal/counties/`)
+      setCountyItems(schools.data.map((school: any) => ({id: school.code, label: school.name})))
+    }
+    fetchCounties()
+  }, [])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const counties = fields.county ? await axios.get(`/api/personal/districts/?county=${fields.county}`) : null
+      counties && setDistrictItems(counties.data.map((school: any) => ({id: school.code, label: school.name})))
+    }
+    fetchData()
+  }, [fields.county])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const schools = fields.district ? await axios.get(`/api/personal/schools/?district=${fields.district}`) : null
+      schools &&
+        setSchoolItems(
+          schools.data.map((school: any) => ({
+            id: school.code,
+            label: `${school.name} ${school.street}, ${school.city}`,
+          })),
+        )
+    }
+    fetchData()
+  }, [fields.district])
+
   const onSubmit = (data: any) => {
     console.log(data)
   }
@@ -118,8 +159,20 @@ const RegisterForm: React.FC = () => {
         <FormInput control={control} name="first_name" label="Krstné meno" required />
         <FormInput control={control} name="last_name" label="Priezvisko" required />
         <FormInput control={control} name="nickname" label="Prezývka" />
-        <FormInput control={control} name="school" label="Škola" required />
-        <FormSelect control={control} name="year_of_graduation" label="Ročník" options={grades} required />
+        <FormCheckbox control={control} name="school_not" label="Už nie som študent základnej ani strednej školy." />
+        <FormSelect control={control} name="county" label="Kraj školy" options={countyItems} />
+        <FormSelect control={control} name="district" label="Okres školy" options={districtItems} />
+        <FormSelect control={control} name="school" label="Škola" options={schoolsItems} />
+        <FormCheckbox control={control} name="school_not_found" label="Moja škola sa v zozname nenachádza." />
+        {fields.school_not_found && (
+          <FormInput
+            control={control}
+            name="school_info"
+            label="povedz nám, kam chodíš na školu, aby sme ti ju mohli dodatočne pridať"
+            hide="true"
+          />
+        )}
+        {/* <FormSelect control={control} name="year_of_graduation" label="Ročník" options={grades} required /> */}
         <FormInput control={control} name="phone" label="Telefónne číslo" />
         <FormInput control={control} name="parent_phone" label="Telefónne číslo na rodiča" />
         <FormCheckbox control={control} name="gdpr" label="Súhlas so spracovaním osobných údajov" required />
