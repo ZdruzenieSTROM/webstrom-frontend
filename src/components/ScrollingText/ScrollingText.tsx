39f42e6584bd28b641cd3a30f14e610255a6599f
Tomas Kocak <tomas.kocak@gmail.com>
2021-02-23 17:36:49 +0100

    add ScrollingText component

---
+++ src/components/ScrollingText/ScrollingText.tsx
@@ -0,0 +1,18 @@
+import './ScrollingText.scss'
+
+import React from 'react'
+
+import {Marquee} from '../Marquee/Marquee'
+
+export const ScrollingText: React.FC = () => {
+  // text sa bude nacitavat z API ked bude vytvoreny endpoint na to
+  let text =
+    'Matboj sa uskutoční 15. októbra 2021 - Matboj sa uskutoční 15. októbra 2021 - Matboj sa uskutoční 15. októbra 2021'
+  return (
+    <div className="scrolling-text">
+      <Marquee gradient={false} speed={50}>
+        <div className="marquee-text-container">{text}</div>
+      </Marquee>
+    </div>
+  )
+}
