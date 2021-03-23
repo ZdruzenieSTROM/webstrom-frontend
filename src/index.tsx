aaffc0032db4cb7f0469cad0e50bbf72efdf4c3b
Tomáš Kocák <tomas.kocak@gmail.com>
2021-03-23 19:58:56 +0100

    fix axios post request (#15)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import './index.css'
 
+import axios from 'axios'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import {Webstrom} from './Webstrom'
+
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 ReactDOM.render(
   <React.StrictMode>
