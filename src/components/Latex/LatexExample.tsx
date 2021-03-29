66e35d184dd00ea1e3f81f5bba10f4bd89226108
Tomáš Kocák <tomas.kocak@gmail.com>
2021-03-29 03:28:04 +0200

    Latex component (#13)
    * Latex component
    
    * add style for LatexExample
    
    * requested changes

---
+++ src/components/Latex/LatexExample.tsx
@@ -0,0 +1,19 @@
+import './LatexExample.css'
+
+import React, {useState} from 'react'
+
+import {Latex} from './Latex'
+
+export const LatexExample: React.FC = () => {
+  const [tex, setTex] = useState('')
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTex(event.target.value)
+  }
+
+  return (
+    <div className="latex-example-container">
+      <textarea value={tex} onChange={handleInputChange} />
+      <Latex>{tex}</Latex>
+    </div>
+  )
+}
