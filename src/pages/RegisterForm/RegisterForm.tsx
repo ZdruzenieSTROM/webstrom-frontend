16e17b6c4bca6c6235537ad50172e3ef85a584b2
matushl <hlavacik.matus@gmail.com>
2021-03-30 21:15:15 +0200

    Finished registration form and remove unused states

--- src/pages/RegisterForm/RegisterForm.tsx
+++ src/pages/RegisterForm/RegisterForm.tsx
@@ -15,7 +15,7 @@ const defaultValues: {
   district: number | ''
   school: number | ''
   school_not_found: boolean
-  year_of_graduation: number | ''
+  grade: number | ''
   gdpr: boolean
 } = {
   school_not: false,
@@ -23,7 +23,7 @@ const defaultValues: {
   district: '',
   school: '',
   school_not_found: false,
-  year_of_graduation: '',
+  grade: '',
   gdpr: false,
 }
 
@@ -36,8 +36,9 @@ const RegisterForm: React.FC = () => {
   const [districtItems, setDistrictItems] = useState<SelectOption[]>([])
   const [countyItems, setCountyItems] = useState<SelectOption[]>([])
 
-  const [emptyDistrictItems, setEmptyDistrictItems] = useState<SelectOption[]>([])
   const [emptySchoolItems, setEmptySchoolItems] = useState<SelectOption[]>([])
+
+  const [successfullRegistration, setSuccessfullRegistration] = useState<string>('')
 
   // načítanie ročníkov z BE, ktorými vyplníme FormSelect s ročníkmi
   useEffect(() => {
@@ -51,8 +52,6 @@ const RegisterForm: React.FC = () => {
   // načítanie ročníkov z BE, ktorými vyplníme FormSelect s ročníkmi
   useEffect(() => {
     const fetchData = async () => {
-      const districts = await axios.get(`/api/personal/districts/?county=0`)
-      setEmptyDistrictItems(districts.data.map((districts: any) => ({id: districts.code, label: districts.name})))
       const schools = await axios.get(`/api/personal/schools/?district=0`)
       setEmptySchoolItems(schools.data.map((school: any) => ({id: school.code, label: school.name})))
     }
@@ -99,89 +98,113 @@ const RegisterForm: React.FC = () => {
       setValue('school', 0)
     } else if (fields.school_not) {
       setValue('county', 0)
-      setValue('year_of_graduation', 13)
+      setValue('grade', 13)
     } else {
       setValue('county', '')
       setValue('district', '')
       setValue('school', '')
-      setValue('year_of_graduation', '')
+      setValue('grade', '')
     }
-  }, [fields.school_not_found, fields.school_not, emptyDistrictItems, emptySchoolItems, setValue])
+  }, [fields.school_not_found, fields.school_not, emptySchoolItems, setValue])
+
+  const transformFormData = (data: any) => {
+    return {
+      email: data.email,
+      password1: data.password1,
+      password2: data.password2,
+      profile: {
+        first_name: data.first_name,
+        last_name: data.last_name,
+        nickname: data.nickname,
+        school: data.school,
+        phone: data.phone,
+        parent_phone: data.parent_phone,
+        gdpr: data.gdpr,
+        grade: data.grade,
+      },
+      new_school_description: data.new_school_description || '',
+    }
+  }
 
   const onSubmit = async (data: any) => {
-    console.log(data)
-    axios.post(`/api/user/registration/`, data).then(
-      (response) => {
-        console.log('response', response)
-      },
-      (error) => {
-        console.log('error', error)
-      },
-    )
+    try {
+      const response = await axios.post(`/api/user/registration/`, transformFormData(data))
+      setSuccessfullRegistration(response.data.detail)
+    } catch (error) {
+      console.log('error', error)
+    }
   }
 
   return (
     <div className="registerform">
       <h1>Registrácia</h1>
-
-      <form>
-        <FormInput control={control} name="email" label="Email" />
-        <FormInput control={control} name="password1" label="Heslo" type="password" />
-        <FormInput control={control} name="password2" label="Potvrdenie hesla" type="password" />
-        <FormInput control={control} name="first_name" label="Krstné meno" />
-        <FormInput control={control} name="last_name" label="Priezvisko" />
-        <FormInput control={control} name="nickname" label="Prezývka" />
-        <FormCheckbox control={control} name="school_not" label="Už nie som študent základnej ani strednej školy." />
-        <FormSelect
-          control={control}
-          name="county"
-          label="Kraj školy"
-          options={countyItems}
-          disabled={fields.school_not_found || fields.school_not}
-        />
-        <FormSelect
-          control={control}
-          name="district"
-          label="Okres školy"
-          options={districtItems}
-          disabled={!districtItems.length || fields.school_not_found || fields.school_not}
-        />
-        <FormSelect
-          control={control}
-          name="school"
-          label="Škola"
-          options={schoolItems}
-          disabled={!schoolItems.length || fields.school_not_found || fields.school_not}
-        />
-        <FormCheckbox
-          control={control}
-          name="school_not_found"
-          label="Moja škola sa v zozname nenachádza."
-          disabled={fields.district === '' || fields.school_not}
-        />
-        {fields.school_not_found && (
-          <FormInput
-            control={control}
-            name="school_info"
-            label="povedz nám, kam chodíš na školu, aby sme ti ju mohli dodatočne pridať"
-          />
-        )}
-        <FormSelect
-          control={control}
-          name="year_of_graduation"
-          label="Ročník"
-          options={gradeItems}
-          disabled={fields.school_not}
-        />
-        <FormInput control={control} name="phone" label="Telefónne číslo" />
-        <FormInput control={control} name="parent_phone" label="Telefónne číslo na rodiča" />
-        <FormCheckbox control={control} name="gdpr" label="Súhlas so spracovaním osobných údajov" />
-      </form>
-
-      <br />
-      <Button variant="contained" color="primary" onClick={handleSubmit(onSubmit)}>
-        Registrovať
-      </Button>
+      {successfullRegistration ? (
+        <p>{successfullRegistration}</p>
+      ) : (
+        <>
+          <form>
+            <FormInput control={control} name="email" label="Email" />
+            <FormInput control={control} name="password1" label="Heslo" type="password" />
+            <FormInput control={control} name="password2" label="Potvrdenie hesla" type="password" />
+            <FormInput control={control} name="first_name" label="Krstné meno" />
+            <FormInput control={control} name="last_name" label="Priezvisko" />
+            <FormInput control={control} name="nickname" label="Prezývka" />
+            <FormCheckbox
+              control={control}
+              name="school_not"
+              label="Už nie som študent základnej ani strednej školy."
+            />
+            <FormSelect
+              control={control}
+              name="county"
+              label="Kraj školy"
+              options={countyItems}
+              disabled={fields.school_not_found || fields.school_not}
+            />
+            <FormSelect
+              control={control}
+              name="district"
+              label="Okres školy"
+              options={districtItems}
+              disabled={!districtItems.length || fields.school_not_found || fields.school_not}
+            />
+            <FormSelect
+              control={control}
+              name="school"
+              label="Škola"
+              options={schoolItems}
+              disabled={!schoolItems.length || fields.school_not_found || fields.school_not}
+            />
+            <FormCheckbox
+              control={control}
+              name="school_not_found"
+              label="Moja škola sa v zozname nenachádza."
+              disabled={fields.district === '' || fields.school_not}
+            />
+            {fields.school_not_found && (
+              <FormInput
+                control={control}
+                name="new_school_description"
+                label="povedz nám, kam chodíš na školu, aby sme ti ju mohli dodatočne pridať"
+              />
+            )}
+            <FormSelect
+              control={control}
+              name="grade"
+              label="Ročník"
+              options={gradeItems}
+              disabled={fields.school_not}
+            />
+            <FormInput control={control} name="phone" label="Telefónne číslo" />
+            <FormInput control={control} name="parent_phone" label="Telefónne číslo na rodiča" />
+            <FormCheckbox control={control} name="gdpr" label="Súhlas so spracovaním osobných údajov" />
+          </form>
+          <br />
+          <Button variant="contained" color="primary" onClick={handleSubmit(onSubmit)}>
+            Registrovať
+          </Button>
+        </>
+      )}
     </div>
   )
 }
