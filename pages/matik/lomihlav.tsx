b4275d48e89d0a27b1637a1229cf7311cc260357
rtrembecky <rtrembecky@gmail.com>
2021-08-01 13:00:50 +0200

    move PagePlaceholder to components/

--- pages/matik/lomihlav.tsx
+++ pages/matik/lomihlav.tsx
@@ -1,7 +1,7 @@
 import {NextPage} from 'next'
 
 import {PageLayout} from '../../src/components/PageLayout/PageLayout'
-import {PagePlaceholder} from '../../src/pages/PagePlaceholder'
+import {PagePlaceholder} from '../../src/components/PagePlaceholder'
 
 const Lomihlav: NextPage = () => (
   <PageLayout seminarId={2}>
--- pages/strom/tmm.tsx
+++ pages/strom/tmm.tsx
@@ -1,7 +1,7 @@
 import {NextPage} from 'next'
 
 import {PageLayout} from '../../src/components/PageLayout/PageLayout'
-import {PagePlaceholder} from '../../src/pages/PagePlaceholder'
+import {PagePlaceholder} from '../../src/components/PagePlaceholder'
 
 const Tmm: NextPage = () => (
   <PageLayout seminarId={1}>
