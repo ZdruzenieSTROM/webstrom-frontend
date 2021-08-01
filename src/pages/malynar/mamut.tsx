c01e8ae58b3a6741227cffc223773314883da842
rtrembecky <rtrembecky@gmail.com>
2021-08-01 13:46:01 +0200

    move pages/ to src/pages/

--- src/pages/malynar/mamut.tsx
+++ src/pages/malynar/mamut.tsx
@@ -1,7 +1,7 @@
 import {NextPage} from 'next'
 
-import {PageLayout} from '../../src/components/PageLayout/PageLayout'
-import {PagePlaceholder} from '../../src/components/PagePlaceholder'
+import {PageLayout} from '../../components/PageLayout/PageLayout'
+import {PagePlaceholder} from '../../components/PagePlaceholder'
 
 const Mamut: NextPage = () => (
   <PageLayout seminarId={3}>
