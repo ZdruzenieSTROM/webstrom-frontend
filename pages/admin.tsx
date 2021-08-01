83c48c08f317e9b48a7955cbaa81d3180d723337
rtrembecky <rtrembecky@gmail.com>
2021-08-01 13:04:16 +0200

    move Admin to components/

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic'
 
 // admin musi byt renderovany client-side: https://nextjs.org/docs/advanced-features/dynamic-import#with-no-ssr
 // eslint-disable-next-line @typescript-eslint/ban-types
-const AdminWithNoSSR = dynamic<{}>(() => import('../src/pages/Admin/Admin').then((mod) => mod.Admin), {ssr: false})
+const AdminWithNoSSR = dynamic<{}>(() => import('../src/components/Admin/Admin').then((mod) => mod.Admin), {ssr: false})
 
 const Admin: NextPage = () => <AdminWithNoSSR />
 
