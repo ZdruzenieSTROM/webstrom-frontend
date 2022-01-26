e7c6be3a874fbc10476991e6cc1fe71181714d41
Tomas Kocak <tomas.kocak@gmail.com>
2022-01-26 23:33:18 +0100

    added zadania (strom) page

--- src/pages/strom/zadania/[[...params]].tsx
+++ src/pages/strom/zadania/[[...params]].tsx
@@ -1,14 +1,32 @@
 import {NextPage} from 'next'
+import {NextRouter, useRouter} from 'next/router'
+import {useState} from 'react'
 
 import {PageLayout} from '@/components/PageLayout/PageLayout'
 import {Problems} from '@/components/Problems/Problems'
 
 const Zadania: NextPage = () => {
+  const [pageTitle, setPageTitle] = useState('')
+  const router: NextRouter = useRouter()
+
   return (
-    <PageLayout contentWidth={3} title={'Zadania'}>
-      <Problems />
+    <PageLayout contentWidth={2} title={pageTitle}>
+      <Problems seminarId={getSeminarId(router.pathname)} setPageTitle={setPageTitle} />
     </PageLayout>
   )
 }
 
 export default Zadania
+
+const getSeminarId = (pathname: string) => {
+  switch (pathname.slice(1).split('/', 1)[0]) {
+    case 'strom':
+      return 0
+    case 'matik':
+      return 1
+    case 'malynar':
+      return 2
+    default:
+      return -1
+  }
+}
