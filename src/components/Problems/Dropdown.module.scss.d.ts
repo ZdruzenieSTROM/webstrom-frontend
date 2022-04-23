5fb47177fbe63c2350e402012af7a96eba2f3d2e
rtrembecky <rtrembecky@gmail.com>
2022-04-23 15:49:02 +0200

    `yarn css-types`

---
+++ src/components/Problems/Dropdown.module.scss.d.ts
@@ -0,0 +1,14 @@
+export type Styles = {
+  arrow: string
+  displayOptions: string
+  dropdown: string
+  option: string
+  options: string
+  selectedOption: string
+}
+
+export type ClassNames = keyof Styles
+
+declare const styles: Styles
+
+export default styles
