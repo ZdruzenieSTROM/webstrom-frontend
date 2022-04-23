cfcca26f7692e9cabd1191db7e1acd3b6b741add
Viktória Brezinová <vikibrezinova@gmail.com>
2022-04-23 21:00:59 +0200

    add competition pages for matik and malynar

---
+++ src/pages/matik/sutaze/[[...params]].tsx
@@ -0,0 +1,5 @@
+import StaticPage, {competitionBasedGetServerSideProps} from '../../strom/sutaze/[[...params]]'
+
+export default StaticPage
+
+export const getServerSideProps = competitionBasedGetServerSideProps('matik')
