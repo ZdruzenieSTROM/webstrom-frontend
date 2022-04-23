2f46b9958a828035f37818f3dd63c312a4aa23e9
matushl <hlavacik.matus@gmail.com>
2022-04-23 21:27:50 +0200

    Oprava console warningov

--- src/components/FormItems/styles.ts
+++ src/components/FormItems/styles.ts
@@ -1,6 +1,6 @@
 export const formItemStyle = {
   '.MuiFormHelperText-contained': {color: '#d32f2f !important'},
   '.MuiInputLabel-root.MuiInputLabel-shrink': {color: 'black'},
-  '.MuiOutlinedInput-root': {'border-radius': 0},
-  '.MuiOutlinedInput-notchedOutline': {'border-width': '2px', 'border-color': 'black'},
+  '.MuiOutlinedInput-root': {borderRadius: 0},
+  '.MuiOutlinedInput-notchedOutline': {borderWidth: '2px', borderColor: 'black'},
 }
