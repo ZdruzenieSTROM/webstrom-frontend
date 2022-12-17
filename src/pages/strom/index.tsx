67e80cce1a9fe11a3b54b668279df7f804371934
Richard Trembecký <rtrembecky@gmail.com>
2022-12-17 13:45:57 +0100

    remove `contentWidth` on `/strom` for consistency (#65)

--- src/pages/strom/index.tsx
+++ src/pages/strom/index.tsx
@@ -4,7 +4,7 @@ import {PageLayout} from '@/components/PageLayout/PageLayout'
 import {Posts} from '@/components/Post/Post'
 
 const Strom: NextPage = () => (
-  <PageLayout contentWidth={1} title="Seminár Strom">
+  <PageLayout title="Seminár Strom">
     <Posts seminarId={1} />
   </PageLayout>
 )
