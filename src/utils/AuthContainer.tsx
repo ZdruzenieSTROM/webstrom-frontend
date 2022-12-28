51c2e5ce58d0df7cea4d962bf94f5d9f19084b3d
rtrembecky <rtrembecky@gmail.com>
2022-12-28 22:09:37 +0100

    better document 403 interceptor

--- src/utils/AuthContainer.tsx
+++ src/utils/AuthContainer.tsx
@@ -40,24 +40,31 @@ const useAuth = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  // 403 (Forbidden) mozeme dostat, ked pristupujeme k niecomu, na co:
+  // - treba auth, ale my nie sme prihlaseni
+  // - treba auth a aj mame prava, ale nasa auth vyprsala (stare/vadne session id)
+  // - treba auth a sice sme prihlaseni, ale nemame prava (napr. snaha o prihlasenie sa do stareho semestra)
   useEffect(() => {
     if (isAuthed) {
-      // interceptor pre 403, ked by sme mali byt authed
+      // ked sme authed a dostaneme 403, chceme overit, ci nam vyprsalo prihlasenie - ak hej, chceme odhlasit usera, nech vie, co sa deje
       const responseInterceptor = axios.interceptors.response.use(
         (response) => response,
         async (error: AxiosError) => {
           const status = error.response?.status
 
-          // sessionid moze byt neaktualne alebo vadne - vráti to 403
           if (status === 403) {
-            // overíme čí nám vypršalo prihlásenie alebo len nemáme práva
+            // bud nemame prava, alebo nam vyprsala auth
+            // auth requestom na profil zistime, co z toho
             const success = await fetchProfile()
 
+            // ak zlyha, tak nam vyprsala auth
             if (!success) {
               // odhlásime usera z UI
               setIsAuthed(false)
               resetProfile()
             }
+            // ak prejde, tak sme len requestovali resource, na ktory nemame prava, ale sme stale validne prihlaseni,
+            // tak netreba robit nic. appka nam snad da vediet, ze sa hrabeme, kam nemame
           }
 
           return Promise.reject(error)
