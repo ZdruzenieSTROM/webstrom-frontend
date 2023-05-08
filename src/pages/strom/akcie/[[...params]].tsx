eaa971fd5a6123f062a89c8399d1a01b4fe385cb
Richard Trembecký <rtrembecky@gmail.com>
2023-05-08 16:11:18 +0200

    Upgrade dependencies 08-05-2023 - next, eslint, other minor ones (#99)
    * add some yarn plugins
    
    * `yarn upgrade-interactive` and upgrade eslint and prettier related packages
    
    * remove naming-convention rule
    
    * autofix some prettier issues
    
    * upgrade next and react deps
    
    * upgrade all packages with minor version changes
    
    * fix ts issue

--- src/pages/strom/akcie/[[...params]].tsx
+++ src/pages/strom/akcie/[[...params]].tsx
@@ -69,37 +69,37 @@ const StaticPage: NextPage<CompetitionPageProps> = ({competition, is_rules}) => 
 export default StaticPage
 
 // wrapper aby sme to lahko vyuzili pre ostatne seminare a neduplikovali kod
-export const competitionBasedGetServerSideProps = (
-  seminar: Seminar,
-): GetServerSideProps<CompetitionPageProps> => async ({query}) => {
-  const redirectToSeminar = {redirect: {destination: `/${seminar}`, permanent: false}}
+export const competitionBasedGetServerSideProps =
+  (seminar: Seminar): GetServerSideProps<CompetitionPageProps> =>
+  async ({query}) => {
+    const redirectToSeminar = {redirect: {destination: `/${seminar}`, permanent: false}}
 
-  // `params` vychadza z nazvu suboru `[[...params]]`
-  // tento check je hlavne pre typescript - parameter `params` by vzdy mal existovat a mal by byt typu string[]
-  if (query?.params && Array.isArray(query.params) && query.params.length > 0) {
-    const requestedUrl = query.params[0]
+    // `params` vychadza z nazvu suboru `[[...params]]`
+    // tento check je hlavne pre typescript - parameter `params` by vzdy mal existovat a mal by byt typu string[]
+    if (query?.params && Array.isArray(query.params) && query.params.length > 0) {
+      const requestedUrl = query.params[0]
 
-    try {
-      const {data} = await axios.get<OurCompetition | undefined>(
-        `${process.env.NEXT_PUBLIC_BE_URL}/competition/competition/${requestedUrl}`,
-      )
-      if (!data) return redirectToSeminar
+      try {
+        const {data} = await axios.get<OurCompetition | undefined>(
+          `${process.env.NEXT_PUBLIC_BE_URL}/competition/competition/${requestedUrl}`,
+        )
+        if (!data) return redirectToSeminar
 
-      if (query.params.length === 2 && query.params[1] === 'pravidla') {
-        if (!data.rules) {
-          return {redirect: {destination: `/${seminar}/akcie/${requestedUrl}`, permanent: false}}
+        if (query.params.length === 2 && query.params[1] === 'pravidla') {
+          if (!data.rules) {
+            return {redirect: {destination: `/${seminar}/akcie/${requestedUrl}`, permanent: false}}
+          }
+          return {props: {competition: data, is_rules: true}}
         }
-        return {props: {competition: data, is_rules: true}}
+
+        return {props: {competition: data, is_rules: false}}
+      } catch (e: unknown) {
+        return redirectToSeminar
       }
-
-      return {props: {competition: data, is_rules: false}}
-    } catch (e: unknown) {
-      return redirectToSeminar
     }
-  }
 
-  return redirectToSeminar
-}
+    return redirectToSeminar
+  }
 
 export const getServerSideProps = competitionBasedGetServerSideProps('strom')
 
