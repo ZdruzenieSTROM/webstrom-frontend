e21db6cf8122d3886900cb4cea03d856f01fc83d
Tomáš Kocák <tomas.kocak@gmail.com>
2023-05-27 11:03:18 +0200

    VerifyEmail uz pouziva useMutation (#105)

--- src/components/VerifyEmail/VerifyEmail.tsx
+++ src/components/VerifyEmail/VerifyEmail.tsx
@@ -1,27 +1,19 @@
-import axios, {AxiosError} from 'axios'
+import {useMutation} from '@tanstack/react-query'
+import axios from 'axios'
 import {useRouter} from 'next/router'
-import {FC, useEffect, useState} from 'react'
+import {FC, useEffect} from 'react'
 
 export const VerifyEmail: FC = () => {
   const router = useRouter()
   const {verificationKey} = router.query
-  const [response, setResponse] = useState('')
+
+  const {mutate: verifyEmail, isSuccess: isEmailVerified} = useMutation({
+    mutationFn: (verificationKey: string) => axios.post('/api/user/registration/verify-email', {key: verificationKey}),
+  })
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await axios.post('/api/user/registration/verify-email', {key: verificationKey})
-        const response =
-          data.status === 200 ? 'Email successfully verified' : 'An unexpected response status code has occurred'
-        setResponse(response)
-      } catch (e: unknown) {
-        const ex = e as AxiosError
-        const error = ex.response?.status === 404 ? 'Wrong verification key' : 'An unexpected error has occurred'
-        setResponse(error)
-      }
-    }
-    fetchData()
-  }, [verificationKey])
+    typeof verificationKey === 'string' && verifyEmail(verificationKey)
+  }, [verificationKey, verifyEmail])
 
-  return <>{response}</>
+  return <>isEmailVerified = {String(isEmailVerified)}</>
 }
