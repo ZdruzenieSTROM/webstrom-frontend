0c30b1afe4a2428703b0f35ba75c3e1aa2830d49
matushl <hlavacik.matus@gmail.com>
2023-05-28 00:05:31 +0200

    update design of inputs to be more up to figma

--- src/components/FormItems/styles.ts
+++ src/components/FormItems/styles.ts
@@ -3,5 +3,5 @@ export const formItemStyle = {
   '.MuiInputLabel-root.MuiInputLabel-shrink': {color: 'black', fontWeight: 'bold'},
   '.MuiInputLabel-root.MuiInputLabel-shrink.Mui-disabled': {color: 'rgba(0, 0, 0, 0.38)'},
   '.MuiOutlinedInput-root': {borderRadius: 0},
-  '.MuiOutlinedInput-notchedOutline': {borderWidth: '2px', borderColor: 'black', fontWeight: 'bold'},
+  '.MuiOutlinedInput-notchedOutline': {borderWidth: '5px', borderColor: 'black', fontWeight: 'bold'},
 }
