de5e3df1b8f1b417dcaac301bd0cc581f98e6199
matushl <hlavacik.matus@gmail.com>
2023-05-28 12:29:58 +0200

    change style of error messages

--- src/components/FormItems/styles.ts
+++ src/components/FormItems/styles.ts
@@ -1,5 +1,5 @@
 export const formItemStyle = {
-  '.MuiFormHelperText-contained': {color: '#d32f2f !important'},
+  '.MuiFormHelperText-contained': {color: 'black !important', fontWeight: 'bold', textTransform: 'uppercase'},
   '.MuiInputLabel-root.MuiInputLabel-shrink': {color: 'black', fontWeight: 'bold'},
   '.MuiInputLabel-root.MuiInputLabel-shrink.Mui-disabled': {color: 'rgba(0, 0, 0, 0.38)'},
   '.MuiOutlinedInput-root': {borderRadius: 0},
