c4126a183a2d86b0857e5abad4dd59de288dce9e
Richard Trembecký <rtrembecky@gmail.com>
2023-11-10 17:11:47 +0100

    Otvaranie PDF nahrateho/opraveneho riesenia v novom tabe (#152)
    * otvorit PDF nahrateho/opraveneho riesenia v novom tabe
    
    * open rest of the PDF links in a new tab

--- src/components/Problems/Problems.tsx
+++ src/components/Problems/Problems.tsx
@@ -72,14 +72,23 @@ const Problem: FC<{
       )}
       {displayActions && (
         <div className={styles.actions}>
-          {problem.solution_pdf && <Link href={problem.solution_pdf}>vzorové riešenie</Link>}
+          {problem.solution_pdf && (
+            <Link href={problem.solution_pdf} target="_blank">
+              vzorové riešenie
+            </Link>
+          )}
           {registered && (
             <>
-              <Link href={`/api/competition/problem/${problem.id}/my-solution`} disabled={!problem.submitted}>
+              <Link
+                href={`/api/competition/problem/${problem.id}/my-solution`}
+                target="_blank"
+                disabled={!problem.submitted}
+              >
                 moje riešenie
               </Link>
               <Link
                 href={`/api/competition/problem/${problem.id}/corrected-solution`}
+                target="_blank"
                 disabled={!problem.submitted?.corrected_solution}
               >
                 opravené riešenie{!!problem.submitted?.corrected_solution && ` (${problem.submitted.score || '?'})`}
