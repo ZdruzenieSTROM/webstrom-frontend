555c019f1bb6612399a013431707a9c629e71479
Viktória Brezinová <vikibrezinova@gmail.com>
2023-11-11 14:45:17 +0100

    change style names

--- src/pages/strom/akcie/competition.module.scss.d.ts
+++ src/pages/strom/akcie/competition.module.scss.d.ts
@@ -1,8 +1,8 @@
 export type Styles = {
-  archive_without_publications: string
-  archive_with_publications: string
   actionButton: string
   actions: string
+  archiveWithoutPublications: string
+  archiveWithPublications: string
   commentActions: string
   container: string
   disabled: string
