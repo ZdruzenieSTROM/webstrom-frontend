3d6f7bef6d2e9fe3e9dd6664a468e4fefc8ce794
rtrembecky <rtrembecky@gmail.com>
2023-11-11 23:54:31 +0100

    adjust MyDateInput as of date format currently coming from BE

--- src/components/Admin/custom/MyDateInput.tsx
+++ src/components/Admin/custom/MyDateInput.tsx
@@ -2,8 +2,13 @@ import {DateTime} from 'luxon'
 import {ComponentProps, FC} from 'react'
 import {DateInput} from 'react-admin'
 
+const backendFormat = 'dd.MM.yyyy HH:mm:ss'
+
+// used to convert BE weird format of 01.01.2020 18:00:00 to 01-01-2020 input format
 // https://moment.github.io/luxon/#/formatting
-const dateFormatter = (v: string) => DateTime.fromISO(v).toISODate()
+const dateFormatter = (v: string) => DateTime.fromFormat(v, backendFormat).toISODate()
+
+// used to convert input format of 01-01-2020 to 01-01-2020TT00:00:00.000+02:00 BE-expected format
 // https://moment.github.io/luxon/#/parsing
 const dateParser = (v: string) => DateTime.fromISO(v).toISO()
 
