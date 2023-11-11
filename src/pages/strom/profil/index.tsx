9640b80a6025c470e31b67738006b2a64cd43273
Michal Masrna <m.masrna1@gmail.com>
2023-11-11 19:59:04 +0100

    Profile contentWidth back to 2

--- src/pages/strom/profil/index.tsx
+++ src/pages/strom/profil/index.tsx
@@ -4,7 +4,7 @@ import {PageLayout} from '@/components/PageLayout/PageLayout'
 import {ProfileDetail} from '@/components/Profile/ProfileDetail'
 
 const Profile: NextPage = () => (
-  <PageLayout contentWidth={1} title="Profil">
+  <PageLayout contentWidth={2} title="Profil">
     <ProfileDetail />
   </PageLayout>
 )
