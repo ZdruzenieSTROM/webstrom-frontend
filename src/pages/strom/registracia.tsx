5edc897971942baacbf26e636437f2c33ec9b46d
Michal Masrna <m.masrna1@gmail.com>
2023-11-11 17:13:04 +0100

    RegisterForm content width from 2 to 1

--- src/pages/strom/registracia.tsx
+++ src/pages/strom/registracia.tsx
@@ -4,7 +4,7 @@ import {PageLayout} from '@/components/PageLayout/PageLayout'
 import {RegisterForm} from '@/components/RegisterForm/RegisterForm'
 
 const Register: NextPage = () => (
-  <PageLayout contentWidth={2} title="Registrácia">
+  <PageLayout contentWidth={1} title="Registrácia">
     <RegisterForm />
   </PageLayout>
 )
