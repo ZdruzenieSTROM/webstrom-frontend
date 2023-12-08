e1731f136ecc808f885dc492968906b35a7b7c5e
Viliam Geffert <60324080+vgeffer@users.noreply.github.com>
2023-12-09 00:08:45 +0100

    176 email verification landing page (#254)
    * feat(verification): Added login form to verification page
    
    * feat(verification): added loading state to verification
    
    * feat(verification): fixed eslint complaints
    
    * feat(verification): changes after review
    
    * feat(verification): changed verification err message

--- src/components/VerifyEmail/VerifyEmail.tsx
+++ src/components/VerifyEmail/VerifyEmail.tsx
@@ -1,13 +1,21 @@
+import {Typography} from '@mui/material'
 import {useMutation} from '@tanstack/react-query'
 import axios from 'axios'
 import {useRouter} from 'next/router'
 import {FC, useEffect} from 'react'
 
+import {Loading} from '../Loading/Loading'
+import {LoginForm} from '../PageLayout/LoginForm/LoginForm'
+
 export const VerifyEmail: FC = () => {
   const router = useRouter()
   const {verificationKey} = router.query
 
-  const {mutate: verifyEmail, isSuccess: isEmailVerified} = useMutation({
+  const {
+    mutate: verifyEmail,
+    isError: isError,
+    isSuccess: isVerified,
+  } = useMutation({
     mutationFn: (verificationKey: string) => axios.post('/api/user/registration/verify-email', {key: verificationKey}),
   })
 
@@ -15,5 +23,23 @@ export const VerifyEmail: FC = () => {
     typeof verificationKey === 'string' && verifyEmail(verificationKey)
   }, [verificationKey, verifyEmail])
 
-  return <>isEmailVerified = {String(isEmailVerified)}</>
+  if (isError)
+    return (
+      <Typography variant="body1">
+        Email už bol verifikovaný, alebo nastal iný problém. Skús sa prihlásiť a v prípade problémov skús overiť email
+        znovu alebo nás kontaktuj.
+      </Typography>
+    )
+  if (isVerified)
+    return (
+      <>
+        <Typography variant="body1">Pre dokončenie overenia emailu sa prihlás</Typography>
+        <LoginForm
+          closeOverlay={() => {
+            router.push('/')
+          }}
+        />
+      </>
+    )
+  return <Loading />
 }
