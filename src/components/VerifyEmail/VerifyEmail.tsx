8ea7e785a18fb7f2d8987e1c2e915844837299ca
vgeffer <wi.lo.dev@hotmail.com>
2023-12-08 23:18:53 +0100

    feat(verification): Added login form to verification page

--- src/components/VerifyEmail/VerifyEmail.tsx
+++ src/components/VerifyEmail/VerifyEmail.tsx
@@ -2,6 +2,7 @@ import {useMutation} from '@tanstack/react-query'
 import axios from 'axios'
 import {useRouter} from 'next/router'
 import {FC, useEffect} from 'react'
+import {LoginForm} from '../PageLayout/LoginForm/LoginForm'
 
 export const VerifyEmail: FC = () => {
   const router = useRouter()
@@ -15,5 +16,14 @@ export const VerifyEmail: FC = () => {
     typeof verificationKey === 'string' && verifyEmail(verificationKey)
   }, [verificationKey, verifyEmail])
 
-  return <>isEmailVerified = {String(isEmailVerified)}</>
+
+  if (!isEmailVerified)
+    return <>I am a temporary email verification error. Please put me out of my misery.</>
+
+  return (
+    <>
+      <p>Pre dokončenie overenia emailu sa prihláste</p>
+      <LoginForm closeOverlay={()=>{ router.push("/") }}/>
+    </>
+  )
 }
