81730a6776037e30ecfbea4ca60e30c8d5216181
Viktória Brezinová <vikibrezinova@gmail.com>
2023-12-09 19:18:02 +0100

    add typography to texts

--- src/components/StaticSites/Texts.tsx
+++ src/components/StaticSites/Texts.tsx
@@ -18,7 +18,23 @@ export const MarkdownLink: FC<MarkdownLinkProps> = ({children, href}) => (
 export const Th: FC<TableHeaderCellProps> = ({children}) => <th className={styles.th}>{children}</th>
 export const Td: FC<TableDataCellProps> = ({children}) => <td className={styles.td}>{children}</td>
 export const Table: FC<ReactMarkdownProps> = ({children}) => <table className={styles.table}>{children}</table>
-export const P: FC<ReactMarkdownProps> = ({children}) => <Typography variant="body1">{children}</Typography>
-export const H1: FC<ReactMarkdownProps> = ({children}) => <Typography variant="h1">{children}</Typography>
-export const H2: FC<ReactMarkdownProps> = ({children}) => <Typography variant="h2">{children}</Typography>
-export const H3: FC<ReactMarkdownProps> = ({children}) => <Typography variant="h3">{children}</Typography>
+export const P: FC<ReactMarkdownProps> = ({children}) => (
+  <Typography variant="body1" sx={{marginTop: 1}} component="div">
+    {children}
+  </Typography>
+)
+export const H1: FC<ReactMarkdownProps> = ({children}) => (
+  <Typography variant="h1" sx={{marginTop: 10}}>
+    {children}
+  </Typography>
+)
+export const H2: FC<ReactMarkdownProps> = ({children}) => (
+  <Typography variant="h2" sx={{marginTop: 5}}>
+    {children}
+  </Typography>
+)
+export const H3: FC<ReactMarkdownProps> = ({children}) => (
+  <Typography variant="h3" sx={{marginTop: 3}}>
+    {children}
+  </Typography>
+)
