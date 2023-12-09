2603e5e1a811da1b8a66b10c6897bc8ee10847e1
matushl <hlavacik.matus@gmail.com>
2023-12-09 22:50:29 +0100

    Fix non-changing title for competitions

--- src/utils/PageTitleContainer.tsx
+++ src/utils/PageTitleContainer.tsx
@@ -1,8 +1,14 @@
 import {useState} from 'react'
 import {createContainer} from 'unstated-next'
+import {useUpdateEffect} from 'usehooks-ts'
 
 const usePageTitle = (initial = '') => {
   const [pageTitle, setPageTitle] = useState(initial)
+
+  useUpdateEffect(() => {
+    setPageTitle(initial)
+  }, [initial])
+
   return {pageTitle, setPageTitle}
 }
 
