0286ef64e8b9138baeadb3c08edfd3ac45692dea
vgeffer <wi.lo.dev@hotmail.com>
2023-12-10 11:53:41 +0100

    feat(admin): changed logout behaviour

--- src/components/Admin/useAuthProvider.ts
+++ src/components/Admin/useAuthProvider.ts
@@ -1,13 +1,18 @@
+import {useRouter} from 'next/router'
 import {AuthProvider, HttpError} from 'react-admin'
 
 import {AuthContainer, testAuth, testAuthRequest} from '@/utils/AuthContainer'
 
 export const useAuthProvider = () => {
-  const {loginAsync, logout} = AuthContainer.useContainer()
+  const {loginAsync, logoutAsync} = AuthContainer.useContainer()
+  const router = useRouter()
 
   const authProvider: AuthProvider = {
     login: async ({username, password}) => loginAsync({data: {email: username, password}}),
-    logout: async () => logout(),
+    logout: async () => {
+      await logoutAsync()
+      router.push('/')
+    },
     checkAuth: async () => {
       await testAuthRequest()
     },
