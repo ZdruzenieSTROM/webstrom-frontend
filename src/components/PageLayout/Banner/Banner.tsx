2bd6c667ad8b8257d5ae628407740e378707768d
rtrembecky <rtrembecky@gmail.com>
2024-01-03 00:45:55 +0100

    rewrite Banner styles using MUI

--- src/components/PageLayout/Banner/Banner.tsx
+++ src/components/PageLayout/Banner/Banner.tsx
@@ -1,20 +1,41 @@
-import {Typography} from '@mui/material'
+import {Box, Typography} from '@mui/material'
 import {FC} from 'react'
 
 import {Marquee} from '@/components/Marquee/Marquee'
 import {BannerContainer} from '@/utils/BannerContainer'
 
-import styles from './Banner.module.scss'
-
 export const Banner: FC = () => {
   const {bannerText} = BannerContainer.useContainer()
+
   return (
-    <div className={styles.banner}>
+    <Box
+      sx={{
+        width: '100vw',
+        bgcolor: 'black',
+        color: 'white',
+        border: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        zIndex: 3,
+        mb: '0.5rem',
+        p: '0.2rem 0',
+      }}
+    >
       <Marquee gradient={false} speed={100}>
-        <Typography variant="h2" component="span" className={styles.marqueeTextContainer}>
+        <Typography
+          variant="h2"
+          component="span"
+          sx={{
+            whiteSpace: 'nowrap',
+            p: 0,
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
           {bannerText || '\u00A0'}
         </Typography>
       </Marquee>
-    </div>
+    </Box>
   )
 }
