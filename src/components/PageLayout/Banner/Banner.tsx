3cbcba979f65b35880d0c055d2c55c21bdb21275
Richard Trembecký <rtrembecky@gmail.com>
2024-01-09 22:12:57 +0100

    Fix horizontal scrollbar, cleanup Banner styles (#326)
    * rewrite Banner styles using MUI
    
    * fix horizontal scrollbar
    
    * cleanup Banner styles

--- src/components/PageLayout/Banner/Banner.tsx
+++ src/components/PageLayout/Banner/Banner.tsx
@@ -1,20 +1,26 @@
-import {Typography} from '@mui/material'
+import {Box, Typography} from '@mui/material'
 import {FC} from 'react'
 
 import {Marquee} from '@/components/Marquee/Marquee'
 import {BannerContainer} from '@/utils/BannerContainer'
 
-import styles from './Banner.module.scss'
-
 export const Banner: FC = () => {
   const {bannerText} = BannerContainer.useContainer()
+
   return (
-    <div className={styles.banner}>
+    <Box
+      sx={{
+        bgcolor: 'black',
+        color: 'white',
+        zIndex: 3,
+        py: '0.2rem',
+      }}
+    >
       <Marquee gradient={false} speed={100}>
-        <Typography variant="h2" component="span" className={styles.marqueeTextContainer}>
+        <Typography variant="h2" component="span" sx={{whiteSpace: 'nowrap'}}>
           {bannerText || '\u00A0'}
         </Typography>
       </Marquee>
-    </div>
+    </Box>
   )
 }
