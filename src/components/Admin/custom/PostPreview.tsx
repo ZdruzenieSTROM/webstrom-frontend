533da23b08ddd784ac6702847558a3d95e072fdd
vgeffer <wi.lo.dev@hotmail.com>
2024-04-13 18:01:01 +0200

    Added details to PostPreview

--- src/components/Admin/custom/PostPreview.tsx
+++ src/components/Admin/custom/PostPreview.tsx
@@ -1,25 +1,32 @@
-import {FC} from 'react'
+import {FC, useState} from 'react'
 import {FormDataConsumer} from 'react-admin'
 
 import {Post} from '@/components/Posts/Post'
+import {PostDetail} from '@/components/Posts/PostDetail'
 
 export const PostPreview: FC = () => {
+  const [isDetailOpen, openDetail] = useState<boolean>()
   return (
     <FormDataConsumer>
       {({formData}) => {
         return (
-          <Post
-            id={0}
-            links={formData?.links ?? []}
-            caption={formData?.caption ?? ''}
-            short_text={formData?.short_text ?? ''}
-            details={formData?.details ?? ''}
-            added_at={formData?.added_at ?? ''}
-            visible_after={formData?.visible_after ?? ''}
-            visible_until={formData?.visible_until ?? ''}
-            sites={formData?.sites ?? []}
-            openDetail={() => {}}
-          />
+          <>
+            <Post
+              id={0}
+              links={formData?.links ?? []}
+              caption={formData?.caption ?? ''}
+              short_text={formData?.short_text ?? ''}
+              details={formData?.details ?? ''}
+              added_at={formData?.added_at ?? ''}
+              visible_after={formData?.visible_after ?? ''}
+              visible_until={formData?.visible_until ?? ''}
+              sites={formData?.sites ?? []}
+              openDetail={() => {
+                openDetail(true)
+              }}
+            />
+            {isDetailOpen && <PostDetail caption={formData?.caption ?? ''} details={formData?.details ?? ''} />}
+          </>
         )
       }}
     </FormDataConsumer>
