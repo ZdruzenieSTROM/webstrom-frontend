29a5aaaee91d4de79502d61eb15e31d3fbd1fd01
vgeffer <wi.lo.dev@hotmail.com>
2024-04-13 21:24:14 +0200

    Fixed comments

--- src/components/Admin/custom/PostPreview.tsx
+++ src/components/Admin/custom/PostPreview.tsx
@@ -8,39 +8,33 @@ import {PostDetail} from '@/components/Posts/PostDetail'
 import {theme} from '@/theme'
 
 export const PostPreview: FC = () => {
-  const [isDetailOpen, openDetail] = useState<boolean>()
+  const [isDetailOpen, openDetail] = useState<boolean>(false)
   return (
     <ThemeProvider theme={theme}>
       <FormDataConsumer>
-        {({formData}) => {
-          return (
-            <>
-              <Grid container>
-                <Grid xs={6}>
-                  <Stack gap={5}>
-                    <Post
-                      id={0}
-                      links={formData?.links ?? []}
-                      caption={formData?.caption ?? ''}
-                      short_text={formData?.short_text ?? ''}
-                      details={formData?.details ?? ''}
-                      added_at={formData?.added_at ?? ''}
-                      visible_after={formData?.visible_after ?? ''}
-                      visible_until={formData?.visible_until ?? ''}
-                      sites={formData?.sites ?? []}
-                      openDetail={() => {
-                        openDetail(true)
-                      }}
-                    />
-                  </Stack>
-                </Grid>
-                <Grid xs={6}>
-                  {isDetailOpen && <PostDetail caption={formData?.caption ?? ''} details={formData?.details ?? ''} />}
-                </Grid>
-              </Grid>
-            </>
-          )
-        }}
+        {({formData}) => (
+          <Grid container>
+            <Grid xs={6}>
+              <Stack gap={5}>
+                <Post
+                  id={0}
+                  links={formData?.links ?? []}
+                  caption={formData?.caption ?? ''}
+                  short_text={formData?.short_text ?? ''}
+                  details={formData?.details ?? ''}
+                  added_at={formData?.added_at ?? ''}
+                  visible_after={formData?.visible_after ?? ''}
+                  visible_until={formData?.visible_until ?? ''}
+                  sites={formData?.sites ?? []}
+                  openDetail={() => openDetail(true)}
+                />
+              </Stack>
+            </Grid>
+            <Grid xs={6}>
+              {isDetailOpen && <PostDetail caption={formData?.caption ?? ''} details={formData?.details ?? ''} />}
+            </Grid>
+          </Grid>
+        )}
       </FormDataConsumer>
     </ThemeProvider>
   )
