b844f98cec2d1bfd3abf535142faf47d7f4a1e58
vgeffer <wi.lo.dev@hotmail.com>
2024-04-13 21:12:58 +0200

    Changed page imports

--- src/pages/malynar/profil/uprava.tsx
+++ src/pages/malynar/profil/uprava.tsx
@@ -1,12 +1,9 @@
 import {NextPage} from 'next'
 
-import {PageLayout} from '@/components/PageLayout/PageLayout'
-import {ProfileForm} from '@/components/Profile/ProfileForm'
+import Page from '../../strom/profil/uprava'
 
-const Profil: NextPage = () => (
-  <PageLayout contentWidth={1} title="Profil">
-    <ProfileForm />
-  </PageLayout>
-)
+const Profil: NextPage = () => {
+  return <Page />
+}
 
 export default Profil
