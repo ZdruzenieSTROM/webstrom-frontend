cd59f4436189076c4784f3ac1f0ac0f6541c3cbc
vgeffer <wi.lo.dev@hotmail.com>
2024-04-15 10:31:06 +0200

    Changed DateTimeField Timezone display

--- src/components/Admin/custom/DateTimeField.tsx
+++ src/components/Admin/custom/DateTimeField.tsx
@@ -1,19 +1,24 @@
+import {Typography} from '@mui/material'
 import {FC} from 'react'
 import {DateField, FieldProps} from 'react-admin'
 
 export const DateTimeField: FC<FieldProps> = ({source}) => (
-  <DateField
-    source={source}
-    showTime={true}
-    options={{
-      timeZone: 'Europe/Bratislava',
-      timeZoneName: 'short',
-      hourCycle: 'h24',
-      hour: '2-digit',
-      minute: '2-digit',
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-    }}
-  />
+  <>
+    <DateField
+      source={source}
+      showTime={true}
+      options={{
+        timeZone: 'Europe/Bratislava',
+        hourCycle: 'h24',
+        hour: '2-digit',
+        minute: '2-digit',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+      }}
+    />
+    <Typography color={'#6F6F6F'} fontSize={'0.75em'}>
+      Europe/Bratislava
+    </Typography>
+  </>
 )
