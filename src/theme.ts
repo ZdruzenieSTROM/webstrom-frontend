13486d95902a9df096d200de3aaba3934b94acbd
rtrembecky <rtrembecky@gmail.com>
2024-05-06 23:32:40 +0700

    unrelated: theme font fallback

--- src/theme.ts
+++ src/theme.ts
@@ -46,10 +46,14 @@ declare module '@mui/material/Typography' {
     subtitle2: false
   }
 }
+
+// font sa stiahne z google fonts pri builde a servuje sa od nas
 export const font = Poppins({
   subsets: ['latin'],
   weight: ['300', '400', '600', '700', '800'],
   display: 'swap',
+  // nemalo by sa stat, ze sa toto pouzije, ale tak aspon bezpatkove pismo nech tam je, keby daco
+  fallback: ['sans-serif'],
 })
 
 const _theme = createTheme({
