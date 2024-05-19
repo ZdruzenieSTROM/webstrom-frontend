963dfa70587caafd04c0a2a75df8eb8bb677bbf6
matushl <hlavacik.matus@gmail.com>
2024-05-19 13:06:45 +0200

    Rewrite CloseButton styles

--- src/components/CloseButton/CloseButton.tsx
+++ src/components/CloseButton/CloseButton.tsx
@@ -1,21 +1,43 @@
-import clsx from 'clsx'
+import {Box, SxProps} from '@mui/material'
 import {FC} from 'react'
 
 import Close from '@/svg/close.svg'
-
-import styles from './CloseButton.module.scss'
 
 interface CloseButtonProps {
   onClick: () => void
   size: number
   invertColors?: boolean
-  className?: string
+  sx?: SxProps
 }
 
-export const CloseButton: FC<CloseButtonProps> = ({onClick, size, invertColors, className}) => {
+export const CloseButton: FC<CloseButtonProps> = ({onClick, size, invertColors, sx}) => {
   return (
-    <div className={clsx(styles.closeButton, invertColors && styles.invertColors, className)} onClick={onClick}>
+    <Box
+      onClick={onClick}
+      sx={{
+        cursor: 'pointer',
+        padding: 0.5,
+        ...(invertColors
+          ? {
+              color: 'black',
+              background: 'white',
+              '&.active, &:hover': {
+                color: 'white',
+                background: 'black',
+              },
+            }
+          : {
+              color: 'white',
+              background: 'black',
+              '&.active, &:hover': {
+                color: 'black',
+                background: 'white',
+              },
+            }),
+        ...sx,
+      }}
+    >
       <Close width={size} height={size} />
-    </div>
+    </Box>
   )
 }
