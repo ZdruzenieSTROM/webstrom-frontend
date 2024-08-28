5444f37a9fa499ad107cc39d5086cd9f70aa2a26
rtrembecky <rtrembecky@gmail.com>
2024-08-28 18:22:50 +0200

    routy opravovania pre Matik a Malynar

---
+++ src/pages/malynar/admin/opravit-ulohu/[[...params]].tsx
@@ -0,0 +1,9 @@
+import {NextPage} from 'next'
+
+import Page from '../../../strom/admin/opravit-ulohu/[[...params]]'
+
+const ProblemAdministration: NextPage = () => {
+  return <Page />
+}
+
+export default ProblemAdministration
---
+++ src/pages/malynar/admin/opravovanie/[[...params]].tsx
@@ -0,0 +1,9 @@
+import {NextPage} from 'next'
+
+import Page from '../../../strom/admin/opravovanie/[[...params]]'
+
+const SemesterAdmnistration: NextPage = () => {
+  return <Page />
+}
+
+export default SemesterAdmnistration
