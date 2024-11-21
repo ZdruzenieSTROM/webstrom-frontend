f10952eca94cd8dcb6b91fcf4b7532425751a6f6
vgeffer <wi.lo.dev@hotmail.com>
2024-11-22 00:36:58 +0100

    Moved timezone info from DateTimeField to footer

--- src/components/Admin/custom/DateTimeField.tsx
+++ src/components/Admin/custom/DateTimeField.tsx
@@ -3,22 +3,17 @@ import {FC} from 'react'
 import {DateField, FieldProps} from 'react-admin'
 
 export const DateTimeField: FC<FieldProps> = ({source}) => (
-  <>
-    <DateField
-      source={source}
-      showTime={true}
-      options={{
-        timeZone: 'Europe/Bratislava',
-        hourCycle: 'h24',
-        hour: '2-digit',
-        minute: '2-digit',
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-      }}
-    />
-    <Typography color={'#6F6F6F'} fontSize={'0.75em'}>
-      Europe/Bratislava
-    </Typography>
-  </>
+  <DateField
+    source={source}
+    showTime={true}
+    options={{
+      timeZone: 'Europe/Bratislava',
+      hourCycle: 'h24',
+      hour: '2-digit',
+      minute: '2-digit',
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+    }}
+  />
 )
