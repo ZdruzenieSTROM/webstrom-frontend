5a13cd027a1be5c7a986c08ff278df6e3b35800f
Michal Masrna <m.masrna1@gmail.com>
2024-11-23 12:46:02 +0100

    Added close button to PostDetail

--- src/components/Posts/PostDetail.tsx
+++ src/components/Posts/PostDetail.tsx
@@ -1,14 +1,16 @@
-import {Stack, Typography} from '@mui/material'
+import {Box, Stack, Typography} from '@mui/material'
 import {FC} from 'react'
 
+import {CloseButton} from '../CloseButton/CloseButton'
 import {PostMarkdown} from './PostMarkdown'
 
 type PostDetailProps = {
   caption: string
   details: string
+  closeDetail: () => void
 }
 
-export const PostDetail: FC<PostDetailProps> = ({caption, details}) => {
+export const PostDetail: FC<PostDetailProps> = ({caption, details, closeDetail: closeDetail}) => {
   return (
     <Stack
       p={2}
@@ -17,8 +19,12 @@ export const PostDetail: FC<PostDetailProps> = ({caption, details}) => {
         backgroundColor: 'white',
         maxHeight: '60vh',
         overflow: 'auto',
+        position: 'relative',
       }}
     >
+      <Box position="absolute" top={16} right={16}>
+        <CloseButton size={30} onClick={closeDetail} invertColors />
+      </Box>
       <Typography variant="postTitle" textTransform="uppercase" fontStyle="italic" mb={4}>
         {caption}
       </Typography>
