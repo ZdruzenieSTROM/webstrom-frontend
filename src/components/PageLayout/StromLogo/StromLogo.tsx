8f309c8d880f7fc64e7e90d44bd0889d4e586a53
matushl <hlavacik.matus@gmail.com>
2024-11-23 12:54:07 +0100

    resolve #383 - main menu will be always open on bigger screen

--- src/components/PageLayout/StromLogo/StromLogo.tsx
+++ src/components/PageLayout/StromLogo/StromLogo.tsx
@@ -1,15 +1,31 @@
-import {FC, MouseEvent} from 'react'
-
-import styles from './StromLogo.module.scss'
-
-const handleClick = (e: MouseEvent<HTMLElement>) => {
-  console.log('Clicked')
-}
+import {Box} from '@mui/material'
+import {FC} from 'react'
 
 export const StromLogo: FC = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.tree} onClick={handleClick} />
-    </div>
+    <Box
+      sx={{
+        position: 'fixed',
+        // tieto cisla su take dost ad-hoc, ale aj tak ten strom nebude vidno
+        top: {md: '204px', lg: '186px', xl: '222px'},
+        left: 0,
+        bottom: 0,
+        width: '25%',
+        paddingTop: 2,
+        zIndex: -1,
+      }}
+    >
+      <Box
+        sx={{
+          height: '100%',
+          width: '100%',
+          backgroundColor: 'black',
+          // toto je stary tomaskov stromcek
+          // clip-path: 'polygon(100% 0%, 100% 100%, 45% 100%, 96% 63%, 55% 63%, 96% 30%, 65% 30%)'
+          // toto je novy stromcek od Matusa Libaka
+          clipPath: 'polygon(100% 0, 100% 100%, 90% 100%, 90% 80%, 35% 80%, 90% 35%, 56% 35%)',
+        }}
+      />
+    </Box>
   )
 }
