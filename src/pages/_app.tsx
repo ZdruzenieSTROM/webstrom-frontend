1ae7b486328a79356ceac8b682cdb60f640214ce
rtrembecky <rtrembecky@gmail.com>
2024-11-23 18:58:59 +0100

    226 - style also mutation error alerts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,14 @@ import {isAxiosError} from 'axios'
 import {AppProps} from 'next/app'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
-import {FC} from 'react'
+import {FC, PropsWithChildren, useMemo} from 'react'
 import {CookiesProvider} from 'react-cookie'
 
 import {AlertBox} from '@/components/Alert/AlertBox'
 import {theme} from '@/theme'
 import {AlertContainer} from '@/utils/AlertContainer'
 import {AuthContainer} from '@/utils/AuthContainer'
+import {useAlert} from '@/utils/useAlert'
 
 const ReactQueryDevtools = dynamic(
   () => import('@tanstack/react-query-devtools').then(({ReactQueryDevtools}) => ReactQueryDevtools),
@@ -73,6 +74,68 @@ const queryClient = new QueryClient({
   },
 })
 
+const ReactQueryProvider: FC<PropsWithChildren> = ({children}) => {
+  const {alert} = useAlert()
+
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: (failureCount, error) => {
+              if (isAxiosError(error)) {
+                // nechceme retryovat 403 (Forbidden)
+                const status = error.response?.status
+                if (status === 403 || status === 404) return false
+              }
+              // klasika - retryuj len 3x
+              if (failureCount >= 3) return false
+              return true
+            },
+          },
+          mutations: {
+            // globalny error handler requestov cez useMutation
+            // notes:
+            // - useMutation vzdy sam loguje error do konzoly, my nemusime
+            // - specifikovanim `onError` na nejakej `useMutation` sa tento handler prepise, tak sa tomu vyhybajme
+            onError: (error) => {
+              if (isAxiosError(error)) {
+                const data = error.response?.data as unknown
+                if (typeof data === 'object' && data) {
+                  // ak mame vlastny message v `.detail`, ukazeme userovi ten
+                  const detail = 'detail' in data && data.detail
+                  if (typeof detail === 'string') {
+                    alert(detail)
+                    return
+                  }
+
+                  // ak nie, ale mame message v `.non_field_errors`, ukazeme ten
+                  const nonFieldErrors = 'non_field_errors' in data && data.non_field_errors
+                  const nonFieldError = Array.isArray(nonFieldErrors) && (nonFieldErrors as unknown[])[0]
+                  if (typeof nonFieldError === 'string') {
+                    alert(nonFieldError)
+                    return
+                  }
+
+                  // TODO: handle field errors (napr. na password) - nealertovat usera, ale zobrazit v UI? alebo alert s prvym field errorom
+
+                  // ak nie, ukazeme kludne original anglicku hlasku z `error`u
+                  alert(error.message)
+                  return
+                }
+              } else {
+                alert(error.message)
+              }
+            },
+          },
+        },
+      }),
+    [alert],
+  )
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+}
+
 const MyApp: FC<AppProps> = ({Component, pageProps}) => {
   return (
     <>
@@ -89,7 +152,7 @@ const MyApp: FC<AppProps> = ({Component, pageProps}) => {
         }
       `}</style>
       <AlertContainer.Provider>
-        <QueryClientProvider client={queryClient}>
+        <ReactQueryProvider>
           <ReactQueryDevtools />
           <CookiesProvider>
             <AuthContainer.Provider>
@@ -99,7 +162,7 @@ const MyApp: FC<AppProps> = ({Component, pageProps}) => {
               </ThemeProvider>
             </AuthContainer.Provider>
           </CookiesProvider>
-        </QueryClientProvider>
+        </ReactQueryProvider>
       </AlertContainer.Provider>
     </>
   )
