eee6995891c1876a995c7a51ed3c48b8b71eef3a
vgeffer <wi.lo.dev@hotmail.com>
2024-11-24 00:39:48 +0100

    Changed to built-in email validation

--- src/components/Admin/resources/personal/schools/SchoolEdit.tsx
+++ src/components/Admin/resources/personal/schools/SchoolEdit.tsx
@@ -1,41 +1,36 @@
 import {FC} from 'react'
-import {AutocompleteInput, ReferenceInput, required, SimpleForm, TextInput} from 'react-admin'
+import {AutocompleteInput, email, ReferenceInput, required, SimpleForm, TextInput} from 'react-admin'
 
 import {MyEdit} from '@/components/Admin/custom/MyEdit'
 
-export const SchoolEdit: FC = () => {
-  const validateEmail = (value: string) => {
-    if (value && !/^[\w%+.-]+@[\d.a-z-]+\.[a-z]{2,}$/iu.test(value)) return 'Zadaj platnú emailovú adresu'
-  }
+export const SchoolEdit: FC = () => (
+  <MyEdit>
+    <SimpleForm>
+      <TextInput
+        fullWidth
+        source="name"
+        helperText="Oficiálny názov školy. Napr. Gymnázium Jura Hronca"
+        validate={required()}
+      />
+      <TextInput fullWidth source="street" validate={required()} />
+      <TextInput fullWidth source="city" validate={required()} />
+      <TextInput fullWidth source="zip_code" helperText="PSČ školy" validate={required()} />
+      <ReferenceInput source="district" reference="personal/districts">
+        <AutocompleteInput fullWidth validate={required()} />
+      </ReferenceInput>
+      <TextInput
+        fullWidth
+        source="abbreviation"
+        helperText="Skratka školy. Zadávajte v tvare napr.: SNov3BA (Druh školy, adresa, číslo, okres)"
+        validate={required()}
+      />
+      <TextInput
+        fullWidth
+        source="email"
+        helperText="Emailový kontakt na školu. Ak neexistuje všeobecný, tak napr. na riaditeľa/vyučujúceho"
+        validate={email()}
+      />
+    </SimpleForm>
+  </MyEdit>
+)
 
-  return (
-    <MyEdit>
-      <SimpleForm>
-        <TextInput
-          fullWidth
-          source="name"
-          helperText="Oficiálny názov školy. Napr. Gymnázium Jura Hronca"
-          validate={required()}
-        />
-        <TextInput fullWidth source="street" validate={required()} />
-        <TextInput fullWidth source="city" validate={required()} />
-        <TextInput fullWidth source="zip_code" helperText="PSČ školy" validate={required()} />
-        <ReferenceInput source="district" reference="personal/districts">
-          <AutocompleteInput fullWidth validate={required()} />
-        </ReferenceInput>
-        <TextInput
-          fullWidth
-          source="abbreviation"
-          helperText="Skratka školy. Zadávajte v tvare napr.: SNov3BA (Druh školy, adresa, číslo, okres)"
-          validate={required()}
-        />
-        <TextInput
-          fullWidth
-          source="email"
-          helperText="Emailový kontakt na školu. Ak neexistuje všeobecný, tak napr. na riaditeľa/vyučujúceho"
-          validate={validateEmail}
-        />
-      </SimpleForm>
-    </MyEdit>
-  )
-}
