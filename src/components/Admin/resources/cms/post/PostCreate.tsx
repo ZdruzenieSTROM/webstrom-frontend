c44da93587ff07c0601b6c648e7eef232a6e3bba
Michal Masrna <38047051+michalmasrna1@users.noreply.github.com>
2024-11-23 13:41:41 +0100

    RA now validates mas length of 200 characters on Post text (#461)

--- src/components/Admin/resources/cms/post/PostCreate.tsx
+++ src/components/Admin/resources/cms/post/PostCreate.tsx
@@ -1,5 +1,5 @@
 import {FC} from 'react'
-import {ArrayInput, FormTab, required, SimpleFormIterator, TabbedForm, TextInput} from 'react-admin'
+import {ArrayInput, FormTab, maxLength, required, SimpleFormIterator, TabbedForm, TextInput} from 'react-admin'
 
 import {MyCreate} from '@/components/Admin/custom/MyCreate'
 import {MyDateTimeInput} from '@/components/Admin/custom/MyDateTimeInput'
@@ -12,7 +12,7 @@ export const PostCreate: FC = () => {
       <TabbedForm>
         <FormTab label="general">
           <TextInput source="caption" fullWidth validate={required()} />
-          <TextInput source="short_text" fullWidth validate={required()} />
+          <TextInput source="short_text" fullWidth validate={maxLength(200, 'Text musí mať najviac 200 znakov.')} />
           <TextInput source="details" multiline fullWidth />
           <MyDateTimeInput source="added_at" fullWidth disabled defaultValue={new Date().toISOString()} />
           <MyDateTimeInput source="visible_after" fullWidth validate={required()} />
