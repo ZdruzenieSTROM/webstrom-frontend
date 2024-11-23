0c1b2606830979e37e7ee2f00e061d821baa547e
vgeffer <wi.lo.dev@hotmail.com>
2024-11-23 23:51:26 +0100

    Season code now maps to strings

--- src/components/Admin/resources/competition/semester/SemesterList.tsx
+++ src/components/Admin/resources/competition/semester/SemesterList.tsx
@@ -12,33 +12,46 @@ import {
 
 import {DateTimeField} from '@/components/Admin/custom/DateTimeField'
 
-export const SemesterList: FC = () => (
-  <List>
-    <Datagrid rowClick="show">
-      <ReferenceField source="competition" reference="competition/competition" link={false} />
-      <NumberField source="year" />
-      <NumberField source="season_code" />
-      <TextField source="school_year" />
-      <DateTimeField source="start" />
-      <DateTimeField source="end" />
-      <BooleanField source="complete" />
-      <TextField source="additional_name" />
-      <NumberField source="registration_link" />
-      <FunctionField<RaRecord>
-        source="series_set"
-        label="Series count"
-        render={(record) => record && <span>{record['series_set'].length}</span>}
-      />
-      <FunctionField<RaRecord>
-        source="publication_set"
-        label="Publication count"
-        render={(record) => record && <span>{record['publication_set'].length}</span>}
-      />
-      <FunctionField<RaRecord>
-        source="late_tags"
-        label="Late tags count"
-        render={(record) => record && <span>{record['late_tags'].length}</span>}
-      />
-    </Datagrid>
-  </List>
-)
+export const SemesterList: FC = () => {
+  const season_code_strings = [
+    {id: 0, name: 'Zimný'},
+    {id: 1, name: 'Letný'},
+    {id: 2, name: 'Iný'},
+  ]
+
+  return (
+    <List>
+      <Datagrid rowClick="show">
+        <ReferenceField source="competition" reference="competition/competition" link={false} />
+        <NumberField source="year" />
+        <FunctionField
+          source="season_code"
+          render={(record) => {
+            return `${season_code_strings[record.season_code].name ?? ''}`
+          }}
+        />
+        <TextField source="school_year" />
+        <DateTimeField source="start" />
+        <DateTimeField source="end" />
+        <BooleanField source="complete" />
+        <TextField source="additional_name" />
+        <NumberField source="registration_link" />
+        <FunctionField<RaRecord>
+          source="series_set"
+          label="Series count"
+          render={(record) => record && <span>{record['series_set'].length}</span>}
+        />
+        <FunctionField<RaRecord>
+          source="publication_set"
+          label="Publication count"
+          render={(record) => record && <span>{record['publication_set'].length}</span>}
+        />
+        <FunctionField<RaRecord>
+          source="late_tags"
+          label="Late tags count"
+          render={(record) => record && <span>{record['late_tags'].length}</span>}
+        />
+      </Datagrid>
+    </List>
+  )
+}
