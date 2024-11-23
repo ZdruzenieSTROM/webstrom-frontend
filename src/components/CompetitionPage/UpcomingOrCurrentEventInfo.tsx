2d8f92883a2b7a6fffe410d20ca1031321ad7e02
Michal Masrna <38047051+michalmasrna1@users.noreply.github.com>
2024-11-23 14:20:41 +0100

    455 using publication types (#468)
    * Introduced PublicationTypes
    
    * UpcomingOrCurrentEvent identifying invitation based on publication_type not index of the publication
    
    * reduce lines a little bit
    
    ---------
    
    Co-authored-by: matushl <hlavacik.matus@gmail.com>

--- src/components/CompetitionPage/UpcomingOrCurrentEventInfo.tsx
+++ src/components/CompetitionPage/UpcomingOrCurrentEventInfo.tsx
@@ -2,7 +2,7 @@ import {Stack, Typography} from '@mui/material'
 import {DateTime} from 'luxon'
 import {FC} from 'react'
 
-import {Event} from '@/types/api/competition'
+import {Event, PublicationTypes} from '@/types/api/competition'
 import {DateFormat, formatDateTimeInterval} from '@/utils/formatDate'
 
 import {Link} from '../Clickable/Link'
@@ -13,6 +13,10 @@ export const UpcomingOrCurrentEventInfo: FC<{event: Event; name: string; shortNa
   shortName,
 }) => {
   const {year, school_year, location, registration_link, publication_set, start, end} = event
+
+  const invitationFile = publication_set.find(
+    (publication) => publication.publication_type === PublicationTypes.INVITATION.id,
+  )
 
   const upcomingEventDate = event ? formatDateTimeInterval(start, end) : null
 
@@ -42,8 +46,8 @@ export const UpcomingOrCurrentEventInfo: FC<{event: Event; name: string; shortNa
         {registrationInfo}
       </Typography>
       <Stack direction="row" sx={{justifyContent: 'end', gap: {xs: 1, sm: 2}}}>
-        {publication_set.length > 0 && (
-          <Link variant="button2" href={`/api/${publication_set[0].file}`}>
+        {invitationFile && (
+          <Link variant="button2" href={`/api/${invitationFile.name}`}>
             Pozvánka
           </Link>
         )}
