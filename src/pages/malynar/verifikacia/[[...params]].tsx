dfa9a46a2c9e789719a2154af01cdb80bf35728a
vgeffer <wi.lo.dev@hotmail.com>
2024-11-24 11:34:56 +0100

    Removed verification landing page

--- src/pages/malynar/verifikacia/[[...params]].tsx
+++
@@ -1,9 +0,0 @@
-import {NextPage} from 'next'
-
-import Page from '../../strom/verifikacia/[[...params]]'
-
-const Verifikacia: NextPage = () => {
-  return <Page />
-}
-
-export default Verifikacia
