2506aa3ff7a619f15e4478f4839d4b7ec8cb160b
rtrembecky <rtrembecky@gmail.com>
2024-12-02 20:08:33 +0100

    add Dark mode toggle button

--- src/components/Admin/AdminLayout.tsx
+++ src/components/Admin/AdminLayout.tsx
@@ -2,7 +2,7 @@ import {Home, Logout} from '@mui/icons-material/'
 import {Button, Stack, Typography} from '@mui/material'
 import {useRouter} from 'next/router'
 import {FC, PropsWithChildren} from 'react'
-import {AppBar, Layout, useLogout} from 'react-admin'
+import {AppBar, Layout, ToggleThemeButton, useLogout} from 'react-admin'
 
 const AppMenuBar = () => {
   const router = useRouter()
@@ -20,6 +20,8 @@ const AppMenuBar = () => {
               <Typography variant="body1">Späť na hlavnú stránku</Typography>
             </Stack>
           </Button>
+
+          <ToggleThemeButton />
 
           <Button color="inherit" onClick={() => logout()}>
             <Stack gap={1} direction="row">
