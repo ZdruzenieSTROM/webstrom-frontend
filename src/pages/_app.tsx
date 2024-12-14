aecf15fee1b004c8e654eac4a62ad1beccc504b1
rtrembecky <rtrembecky@gmail.com>
2024-12-14 21:19:57 +0100

    clean-up - old unused queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,58 +21,6 @@ const ReactQueryDevtools = dynamic(
   },
 )
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: (failureCount, error) => {
-        if (isAxiosError(error)) {
-          // nechceme retryovat 403 (Forbidden)
-          const status = error.response?.status
-          if (status === 403 || status === 404) return false
-        }
-        // klasika - retryuj len 3x
-        if (failureCount >= 3) return false
-        return true
-      },
-    },
-    mutations: {
-      // globalny error handler requestov cez useMutation
-      // notes:
-      // - useMutation vzdy sam loguje error do konzoly, my nemusime
-      // - specifikovanim `onError` na nejakej `useMutation` sa tento handler prepise, tak sa tomu vyhybajme
-      onError: (error) => {
-        if (isAxiosError(error)) {
-          const data = error.response?.data as unknown
-          if (typeof data === 'object' && data) {
-            // ak mame vlastny message v `.detail`, ukazeme userovi ten
-            const detail = 'detail' in data && data.detail
-            if (typeof detail === 'string') {
-              alert(detail)
-              return
-            }
-
-            // ak nie, ale mame message v `.non_field_errors`, ukazeme ten
-            const nonFieldErrors = 'non_field_errors' in data && data.non_field_errors
-            const nonFieldError = Array.isArray(nonFieldErrors) && (nonFieldErrors as unknown[])[0]
-            if (typeof nonFieldError === 'string') {
-              alert(nonFieldError)
-              return
-            }
-
-            // TODO: handle field errors (napr. na password) - nealertovat usera, ale zobrazit v UI? alebo alert s prvym field errorom
-
-            // ak nie, ukazeme kludne original anglicku hlasku z `error`u
-            alert(error)
-            return
-          }
-        } else {
-          alert(error)
-        }
-      },
-    },
-  },
-})
-
 const ReactQueryProvider: FC<PropsWithChildren> = ({children}) => {
   const {alert} = useAlert()
 
