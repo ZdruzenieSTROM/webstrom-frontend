f46db88f0897c8ef0254d990e741fdb30a2dda2c
matushl <hlavacik.matus@gmail.com>
2024-12-14 20:30:39 +0100

    Try no. 1

--- src/components/PageLayout/StromLogo/StromLogo.tsx
+++ src/components/PageLayout/StromLogo/StromLogo.tsx
@@ -1,13 +1,35 @@
-import {Box} from '@mui/material'
-import {FC} from 'react'
+import {Box, Button} from '@mui/material'
+import {FC, useEffect, useRef, useState} from 'react'
 
 import Strom from '@/svg/strom.svg'
+import {BannerContainer} from '@/utils/BannerContainer'
 
 export const StromLogo: FC = () => {
+  const firstBoxRef = useRef<HTMLDivElement>(null)
+  const [topPosition, setTopPosition] = useState(0)
+
+  const {bannerMessages} = BannerContainer.useContainer()
+
+  const updateTopPosition = () => {
+    if (firstBoxRef.current) {
+      const rect = firstBoxRef.current.getBoundingClientRect()
+      setTopPosition(rect.top)
+      console.log(rect.top)
+    }
+  }
+
+  useEffect(() => {
+    updateTopPosition()
+    // toto sa prepocita skor ako sa baner zobrazi v plnej sirke, takze toto uplne nefunguje
+    // dalsim problemom je ked sa zmeni nadpis (napriklad pri poradi prejdes z poradia semestra na seriu - zvacsi sa nadpis - banner sa posunie nizsie)
+  }, [bannerMessages])
+
   return (
     <>
       {/* zistit pozicitu tohto boxu a nastavit to ako top pre box obsahujuci strom */}
-      <Box />
+      <Box ref={firstBoxRef}>
+        <Button onClick={updateTopPosition}>Update top position</Button>
+      </Box>
       <Box
         sx={{
           position: 'fixed',
@@ -16,10 +38,11 @@ export const StromLogo: FC = () => {
           // taktiez pri sirke 900 - 1100px sa niektore nadpisy zalomuju a rozbijaju zarovnanie stromu
           // - problem toho je ze to je rozlisenie vacsinu tabletov na vysku, takze to bude treba fixnut
           // top: {md: '213px', lg: '265px', xl: '301px'},
+          top: topPosition,
           left: 0,
           bottom: 0,
           width: '25%',
-          paddingTop: 2,
+          // paddingTop: 2,
           overflow: 'hidden',
           display: 'flex',
           flexDirection: 'column',
