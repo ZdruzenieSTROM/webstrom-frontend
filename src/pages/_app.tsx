092bfa956966ed29081f43414a3c5d20b50c4f2a
rtrembecky <rtrembecky@gmail.com>
2024-12-14 21:19:38 +0100

    remove index.scss in favor of CssBaseline and some theme overrides

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
-import '../index.scss'
-
+import {CssBaseline} from '@mui/material'
 import {ThemeProvider} from '@mui/material/styles'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {isAxiosError} from 'axios'
@@ -160,6 +159,7 @@ const MyApp: FC<AppProps> = ({Component, pageProps}) => {
           <CookiesProvider>
             <AuthContainer.Provider>
               <ThemeProvider theme={theme}>
+                <CssBaseline />
                 <AlertBox />
                 <Component {...pageProps} />
               </ThemeProvider>
