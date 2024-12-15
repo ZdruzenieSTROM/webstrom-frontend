a8660b6c52d32f75faafd4c9525303d9c33ef35c
vgeffer <wi.lo.dev@hotmail.com>
2024-12-15 11:58:14 +0100

    Unified fields across page

--- src/components/Admin/resources/competition/series/SeriesCreate.tsx
+++ src/components/Admin/resources/competition/series/SeriesCreate.tsx
@@ -1,19 +1,25 @@
 import {FC} from 'react'
-import {BooleanInput, FormTab, ReferenceInput, required, SelectInput, TabbedForm, TextInput} from 'react-admin'
+import {AutocompleteInput, FormTab, NumberInput, ReferenceInput, required, TabbedForm} from 'react-admin'
 
 import {MyCreate} from '@/components/Admin/custom/MyCreate'
 import {MyDateTimeInput} from '@/components/Admin/custom/MyDateTimeInput'
 
 export const SeriesCreate: FC = () => (
-  <MyCreate>
+  <MyCreate
+    transform={(record) => {
+      // automaticky sa na BE posiela cely record, ale BE read_only (aj neexistujuce) fieldy ignoruje
+      // radsej z payloadu odstranime aspon sety
+      delete record.problems
+      return record
+    }}
+  >
     <TabbedForm>
       <FormTab label="content.labels.general">
         <ReferenceInput source="semester" reference="competition/semester">
-          <SelectInput fullWidth validate={required()} />
+          <AutocompleteInput optionText="verbose_name" fullWidth validate={required()} />
         </ReferenceInput>
         <MyDateTimeInput source="deadline" fullWidth validate={required()} />
-        <TextInput source="order" fullWidth validate={required()} />
-        <BooleanInput source="complete" disabled />
+        <NumberInput source="order" fullWidth validate={required()} />
       </FormTab>
     </TabbedForm>
   </MyCreate>
