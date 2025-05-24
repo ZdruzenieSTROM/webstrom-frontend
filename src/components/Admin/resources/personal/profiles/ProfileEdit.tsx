d08ac8112283bbe97ada757b21072d21768dc68e
viktoria.brezinova <viktoria.brezinova@promethist.ai>
2025-05-24 21:19:57 +0200

    add Edit of Profile

--- src/components/Admin/resources/personal/profiles/ProfileEdit.tsx
+++ src/components/Admin/resources/personal/profiles/ProfileEdit.tsx
@@ -1,1 +1,24 @@
-// For now we have decided we do not want to edit user profile in React Admin
+import {FC} from 'react'
+import {AutocompleteInput, ReferenceInput, required, SimpleForm, TextInput} from 'react-admin'
+
+import {MyEdit} from '@/components/Admin/custom/MyEdit'
+
+export const ProfileEdit: FC = () => (
+  <MyEdit
+    transform={(record) => {
+      const {school, ...rest} = record
+      return {...rest, school_id: school.code}
+    }}
+  >
+    <SimpleForm>
+      <TextInput source="first_name" validate={required()} />
+      <TextInput source="last_name" validate={required()} />
+      <ReferenceInput source="school.code" reference="personal/schools">
+        <AutocompleteInput optionText="verbose_name" fullWidth validate={required()} />
+      </ReferenceInput>
+      <ReferenceInput source="grade" reference="competition/grade">
+        <AutocompleteInput optionText="name" fullWidth validate={required()} />
+      </ReferenceInput>
+    </SimpleForm>
+  </MyEdit>
+)
