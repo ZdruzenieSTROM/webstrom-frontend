59a39bb7d4ee8866176d5d89de3ea1d029bffc22
Martin Mihálik <mihalik.martin@outlook.sk>
2025-05-24 20:27:48 +0200

    Tlačidlo na opravovací page konkrétnej úlohy

--- src/components/Problems/Problem.tsx
+++ src/components/Problems/Problem.tsx
@@ -6,6 +6,7 @@ import {Button} from '@/components/Clickable/Button'
 import {Link} from '@/components/Clickable/Link'
 import {Problem as ProblemType} from '@/types/api/competition'
 import {AuthContainer} from '@/utils/AuthContainer'
+import {Seminar} from '@/utils/useSeminarInfo'
 
 import {Markdown} from '../Markdown/Markdown'
 import {UploadProblemForm} from './UploadProblemForm'
@@ -16,21 +17,25 @@ export type DiscussionProblem = {
 }
 
 export const Problem: FC<{
+  seminar: Seminar
   problem: ProblemType
   openDiscussion: (problem: DiscussionProblem) => void
   registered: boolean
   canRegister: boolean
   canSubmit: boolean
+  canCorrect: boolean
   isAfterDeadline: boolean
   invalidateSeriesQuery: () => Promise<void>
   displayRegisterDialog: () => void
   displayLoginDialog: () => void
 }> = ({
+  seminar,
   problem,
   registered,
   openDiscussion,
   canRegister,
   canSubmit,
+  canCorrect,
   isAfterDeadline,
   invalidateSeriesQuery,
   displayRegisterDialog,
@@ -120,10 +125,14 @@ export const Problem: FC<{
                 disabled={!problem.submitted?.corrected_solution}
                 variant="button2"
               >
-                opravené riešenie
-                {problem.submitted?.score != null && ` (${problem.submitted.score} b)`}
+                opravené riešenie{!!problem.submitted?.corrected_solution && ` (${problem.submitted.score || '?'})`}
               </Link>
             </>
+          )}
+          {canCorrect && (
+            <Link href={`/${seminar}/admin/opravit-ulohu/${problem.id}`} disabled={false} variant="button2">
+              Opravovanie
+            </Link>
           )}
           <Button onClick={() => openDiscussion(problem)} variant="button2">
             diskusia ({problem.num_comments})
