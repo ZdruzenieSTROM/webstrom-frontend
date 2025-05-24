2ca92e6a85bf326650621e1ce7e3d85f9c1b0081
rtrembecky <rtrembecky@gmail.com>
2025-05-24 10:01:43 +0200

    fix ReactQueryDevtools in RA

--- src/components/Admin/AdminLayout.tsx
+++ src/components/Admin/AdminLayout.tsx
@@ -1,5 +1,6 @@
 import {Home, Logout} from '@mui/icons-material/'
 import {Button, Stack, Typography} from '@mui/material'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {useRouter} from 'next/router'
 import {FC, PropsWithChildren} from 'react'
 import {AppBar, Layout, ToggleThemeButton, useLogout, useTranslate} from 'react-admin'
@@ -35,11 +36,26 @@ const AppMenuBar = () => {
   )
 }
 
+const footerHeight = '32px'
+
 export const AdminLayout: FC<PropsWithChildren> = ({children}) => {
   const translate = useTranslate()
   return (
     <>
-      <Layout appBar={AppMenuBar}>{children}</Layout>
+      <Layout
+        appBar={AppMenuBar}
+        sx={{
+          pb: footerHeight,
+          // there's no way to style the DevTools directly, we move them up above the footer as well.
+          // another approach would be just changing z-index.
+          // 12px is the hardcoded bottom offset for the DevTools button
+          '.tsqd-open-btn-container': {bottom: `calc(12px + ${footerHeight})`},
+          '.tsqd-main-panel': {bottom: footerHeight},
+        }}
+      >
+        {children}
+        <ReactQueryDevtools />
+      </Layout>
       <Stack
         style={{
           position: 'fixed',
@@ -47,7 +63,8 @@ export const AdminLayout: FC<PropsWithChildren> = ({children}) => {
           bottom: 0,
           left: 0,
           zIndex: 100,
-          padding: 6,
+          height: footerHeight,
+          justifyContent: 'center',
           textAlign: 'center',
           backgroundColor: '#efefef',
         }}
