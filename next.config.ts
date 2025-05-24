4f121f5fd6aa772760569153bfdd11d09c70d8c4
rtrembecky <rtrembecky@gmail.com>
2025-05-24 13:00:25 +0200

    turbopack updates

--- next.config.ts
+++ next.config.ts
@@ -11,49 +11,20 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  reactStrictMode: true,
-  // https://react-svgr.com/docs/next/
-  webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'))
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: {not: /url/}, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    )
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i
-
-    return config
+    },
   },
+  reactStrictMode: true,
   // typecheck aj eslint pustame v CI pred buildom osobitne, nemusi ich pustat aj next
   typescript: {ignoreBuildErrors: true},
   eslint: {ignoreDuringBuilds: true},
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#advanced-middleware-flags
   skipTrailingSlashRedirect: true,
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
 }
 
 // eslint-disable-next-line import/no-default-export
