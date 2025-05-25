d3ceeeb37a9d31d37a9109cb711120411db90625
matushl <hlavacik.matus@gmail.com>
2025-05-25 10:31:08 +0200

    Change "vysledky" to "poradie"

---
+++ src/pages/malynar/poradie/[[...params]].tsx
@@ -0,0 +1,11 @@
+import {NextPage} from 'next'
+
+import Page, {getServerSideProps} from '../../strom/poradie/[[...params]]'
+
+const Poradie: NextPage = () => {
+  return <Page />
+}
+
+export default Poradie
+
+export {getServerSideProps}
