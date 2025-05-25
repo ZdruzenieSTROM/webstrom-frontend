e7bc2970432f228c2d36b6159320c3137eef0481
rtrembecky <rtrembecky@gmail.com>
2025-05-25 11:50:05 +0200

    turbopack - third try. also revisited Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -19,35 +19,7 @@ const nextConfig: NextConfig = {
       },
     },
   },
-  // https://react-svgr.com/docs/next/
-  // skusili sme tento config zmazat, ale rozbilo to produkcny build. treba potom pozriet,
-  // ci turbopack este na prode nefunguje, alebo cim to je.
-  webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'))
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: {not: /url/}, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    )
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i
-
-    return config
-  },
+  output: 'standalone',
   reactStrictMode: true,
   // typecheck aj eslint pustame v CI pred buildom osobitne, nemusi ich pustat aj next
   typescript: {ignoreBuildErrors: true},
