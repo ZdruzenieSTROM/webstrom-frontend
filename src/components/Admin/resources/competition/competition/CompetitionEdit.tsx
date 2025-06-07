6b2d3a3d07f87799d7603fb901f9be2029c04016
rtrembecky <rtrembecky@gmail.com>
2025-06-08 00:27:30 +0200

    /akcie - Podrobnosti - RA

--- src/components/Admin/resources/competition/competition/CompetitionEdit.tsx
+++ src/components/Admin/resources/competition/competition/CompetitionEdit.tsx
@@ -7,15 +7,16 @@ import {SitesCheckboxInput} from '@/components/Admin/custom/SitesCheckboxInput'
 export const CompetitionEdit: FC = () => (
   <MyEdit>
     <SimpleForm>
-      <TextInput source="name" fullWidth disabled />
-      <TextInput source="slug" fullWidth disabled />
-      <NumberInput source="start_year" fullWidth disabled />
-      <TextInput source="description" multiline fullWidth />
-      <TextInput source="rules" multiline fullWidth />
-      <TextInput source="competition_type.name" label="content.labels.competition_type" fullWidth disabled />
+      <TextInput source="name" disabled />
+      <TextInput source="slug" disabled />
+      <NumberInput source="start_year" disabled />
+      <TextInput source="description" multiline />
+      <TextInput source="long_description" multiline />
+      <TextInput source="rules" multiline />
+      <TextInput source="competition_type.name" label="content.labels.competition_type" disabled />
       <SitesCheckboxInput source="sites" disabled />
-      <TextInput source="who_can_participate" fullWidth />
-      <NumberInput source="min_years_until_graduation" fullWidth disabled />
+      <TextInput source="who_can_participate" />
+      <NumberInput source="min_years_until_graduation" disabled />
     </SimpleForm>
   </MyEdit>
 )
