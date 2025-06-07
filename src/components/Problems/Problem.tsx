cfdd1217cd53257364a275b7ca07b96f4b75403d
rtrembecky <rtrembecky@gmail.com>
2025-06-07 17:43:54 +0200

    unrelated: late tag warning icon for Dorot

--- src/components/Problems/Problem.tsx
+++ src/components/Problems/Problem.tsx
@@ -1,4 +1,4 @@
-import {WarningAmber} from '@mui/icons-material'
+import {Warning} from '@mui/icons-material'
 import {Stack, Tooltip, Typography} from '@mui/material'
 import Image from 'next/image'
 import {FC, useState} from 'react'
@@ -126,7 +126,7 @@ export const Problem: FC<{
                 >
                   <Stack direction="row" alignItems="center" gap={0.5} component="span">
                     moje riešenie
-                    {problem.submitted?.late_tag && <WarningAmber sx={{width: '16px', height: '16px'}} />}
+                    {problem.submitted?.late_tag && <Warning sx={{width: '16px', height: '16px'}} />}
                   </Stack>
                 </Tooltip>
               </Link>
