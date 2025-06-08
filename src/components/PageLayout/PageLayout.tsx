786403c0fd5087c94da7412d05a951efeb365663
rtrembecky <rtrembecky@gmail.com>
2025-06-08 12:57:49 +0200

    fuck you BannerContainer 🖕

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -3,7 +3,6 @@ import Grid from '@mui/material/Unstable_Grid2'
 import Head from 'next/head'
 import {FC, ReactNode} from 'react'
 
-import {BannerContainer} from '@/utils/BannerContainer'
 import {PageTitleContainer} from '@/utils/PageTitleContainer'
 import {Seminar, useSeminarInfo} from '@/utils/useSeminarInfo'
 
@@ -40,45 +39,43 @@ export const PageLayout: FC<PageLayoutProps> = ({contentWidth = 2, title = '', b
         <title>{browserTitle}</title>
       </Head>
       <PageTitleContainer.Provider initialState={title}>
-        <BannerContainer.Provider initialState={bannerMessages}>
-          <Stack sx={{minHeight: '100dvh'}}>
-            <TopGrid />
-            <Stack sx={{flex: 1}}>
-              <Box
+        <Stack sx={{minHeight: '100dvh'}}>
+          <TopGrid bannerMessages={bannerMessages} />
+          <Stack sx={{flex: 1}}>
+            <Box
+              sx={{
+                display: {xs: 'none', md: 'block'},
+                pt: horizontalContentPadding,
+                position: 'fixed',
+                width: '25%',
+              }}
+            >
+              <StromLogo />
+            </Box>
+
+            <Grid container disableEqualOverflow sx={{flex: 1}}>
+              <Grid
+                xsOffset={0}
+                mdOffset={3}
+                xs={12}
+                md={contentWidth * 3}
                 sx={{
-                  display: {xs: 'none', md: 'block'},
-                  pt: horizontalContentPadding,
-                  position: 'fixed',
-                  width: '25%',
+                  py: horizontalContentPadding,
+                  px: 2,
+                  ...sx,
+                  // v server-renderi bol v consoli warning, ale first-child je tu asi uplne v pohode selector :D
+                  // https://github.com/emotion-js/emotion/issues/1105#issuecomment-557726922
+                  '> :first-child /* emotion-disable-server-rendering-unsafe-selector-warning-please-do-not-use-this-the-warning-exists-for-a-reason */':
+                    {mt: 0, pt: 0},
                 }}
               >
-                <StromLogo />
-              </Box>
-
-              <Grid container disableEqualOverflow sx={{flex: 1}}>
-                <Grid
-                  xsOffset={0}
-                  mdOffset={3}
-                  xs={12}
-                  md={contentWidth * 3}
-                  sx={{
-                    py: horizontalContentPadding,
-                    px: 2,
-                    ...sx,
-                    // v server-renderi bol v consoli warning, ale first-child je tu asi uplne v pohode selector :D
-                    // https://github.com/emotion-js/emotion/issues/1105#issuecomment-557726922
-                    '> :first-child /* emotion-disable-server-rendering-unsafe-selector-warning-please-do-not-use-this-the-warning-exists-for-a-reason */':
-                      {mt: 0, pt: 0},
-                  }}
-                >
-                  {children}
-                </Grid>
+                {children}
               </Grid>
-            </Stack>
-
-            <Footer />
+            </Grid>
           </Stack>
-        </BannerContainer.Provider>
+
+          <Footer />
+        </Stack>
       </PageTitleContainer.Provider>
     </>
   )
