c070fa900f95bfee2e9d3abd18996bf9db3facfe
rtrembecky <rtrembecky@gmail.com>
2025-09-20 13:24:48 +0200

    use Fira Math for Markdown math

--- src/theme/font.ts
+++ src/theme/font.ts
@@ -1,4 +1,5 @@
 import {Poppins} from 'next/font/google'
+import localFont from 'next/font/local'
 
 // font sa stiahne z google fonts pri builde a servuje sa od nas
 export const poppins = Poppins({
@@ -7,4 +8,12 @@ export const poppins = Poppins({
   display: 'swap',
   // nemalo by sa stat, ze sa toto pouzije, ale tak aspon bezpatkove pismo nech tam je, keby daco
   fallback: ['sans-serif'],
+})
+
+// Fira Math font for mathematical expressions
+export const firaMath = localFont({
+  src: '../../public/fonts/FiraMath-Regular.otf',
+  variable: '--font-fira-math',
+  display: 'swap',
+  fallback: ['KaTeX_Main', 'Times New Roman', 'serif'],
 })
