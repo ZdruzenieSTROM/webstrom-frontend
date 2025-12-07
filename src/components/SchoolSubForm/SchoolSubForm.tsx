d919eda953d128c4d5899f53356666ac5c032a5c
kovacspe <pet.kovacs1@gmail.com>
2025-12-07 02:04:24 +0100

    Registration grade helper text (#782)
    * Doplnený helper text pre výber ročníka v registrácií
    
    * Remove log
    
    * eslint
    
    * Adjust styles to make it nicer
    
    ---------
    
    Co-authored-by: matushl <hlavacik.matus@gmail.com>

--- src/components/SchoolSubForm/SchoolSubForm.tsx
+++ src/components/SchoolSubForm/SchoolSubForm.tsx
@@ -1,4 +1,4 @@
-import {Stack} from '@mui/material'
+import {Stack, Typography} from '@mui/material'
 import {useQuery} from '@tanstack/react-query'
 import {useEffect, useMemo} from 'react'
 import {Control, UseFormSetValue, UseFormWatch} from 'react-hook-form'
@@ -63,7 +63,8 @@ export const SchoolSubForm = ({control, watch, setValue, gap}: SchoolSubFormProp
   const withoutSchoolItem = useMemo(() => emptySchoolItems.find(({id}) => id === 1), [emptySchoolItems])
 
   const isLoading = isGradesLoading || isSchoolsLoading
-
+  const currentMonth = new Date().getMonth()
+  const showSummerHelp = currentMonth === 6 || currentMonth === 7 // Júl alebo August (indexovane od 0)
   useEffect(() => {
     if (isLoading) return
     if (without_school) {
@@ -137,6 +138,21 @@ export const SchoolSubForm = ({control, watch, setValue, gap}: SchoolSubFormProp
         disabled={gradeItems.length === 0 || without_school}
         rules={requiredRule}
       />
+      {showSummerHelp && (
+        <Typography
+          variant="body2"
+          ml={1}
+          mr={2}
+          mt={-1.5}
+          sx={{
+            textTransform: 'uppercase',
+            fontStyle: 'italic',
+            color: gradeItems.length === 0 || without_school ? 'rgba(0, 0, 0, 0.38)' : 'inherit',
+          }}
+        >
+          Ak sa registruješ počas letných prázdnin, zvoľ ročník, ktorý si práve dokončil.
+        </Typography>
+      )}
     </Stack>
   )
 }
