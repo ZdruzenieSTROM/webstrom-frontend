ad6b2f88546d838ebfafba216fc1c5366b95dcf9
rtrembecky <rtrembecky@gmail.com>
2025-12-07 12:39:03 +0100

    unify gaps

--- src/components/Admin/custom/MyCreateToolbar.tsx
+++ src/components/Admin/custom/MyCreateToolbar.tsx
@@ -5,7 +5,7 @@ export const MyCreateToolbar = () => {
   const {reset} = useFormContext()
   const notify = useNotify()
   return (
-    <Toolbar>
+    <Toolbar sx={{gap: 1}}>
       <SaveButton />
       <SaveButton
         label="ra.action.save_and_create_another"
--- src/components/Admin/custom/MyEditToolbar.tsx
+++ src/components/Admin/custom/MyEditToolbar.tsx
@@ -5,7 +5,7 @@ export const MyEditToolbar = () => {
   const notify = useNotify()
   return (
     <Toolbar sx={{justifyContent: 'space-between'}}>
-      <Stack direction="row" spacing={1}>
+      <Stack direction="row" gap={1}>
         <SaveButton />
         <SaveButton
           label="ra.action.save_and_stay"
