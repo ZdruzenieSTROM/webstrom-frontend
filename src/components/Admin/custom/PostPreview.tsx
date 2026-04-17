9e79018feda2a5d3de5a43aba6f53f0613ad07e8
Branislav Ječim <xjecim@stuba.sk>
2026-04-17 23:40:46 +0200

    818: fixed width of post preview to reflect width on Novinky page (#828)
    * 818: fixed width of post preview to reflect width on Novinky page
    
    * remove some unnecessary wrappers
    
    ---------
    
    Co-authored-by: matushl <hlavacik.matus@gmail.com>

--- src/components/Admin/custom/PostPreview.tsx
+++ src/components/Admin/custom/PostPreview.tsx
@@ -1,4 +1,4 @@
-import {Grid, Stack} from '@mui/material'
+import {Stack} from '@mui/material'
 import {ThemeProvider} from '@mui/material/styles'
 import {FC} from 'react'
 import {FormDataConsumer} from 'react-admin'
@@ -12,23 +12,22 @@ export const PostPreview: FC = () => {
     <ThemeProvider theme={theme}>
       <FormDataConsumer>
         {({formData}) => (
-          <Grid container columnSpacing={5} sx={{width: '100%'}}>
-            <Grid size={{xs: 12, md: 4}}>
-              <Stack gap={5} sx={{color: colors.black, background: colors.white, p: '10px'}}>
-                <Post
-                  id={0}
-                  links={formData?.links ?? []}
-                  caption={formData?.caption ?? ''}
-                  short_text={formData?.short_text ?? ''}
-                  details={formData?.details ?? ''}
-                  added_at={formData?.added_at ?? new Date().toISOString()}
-                  visible_after={formData?.visible_after ?? ''}
-                  visible_until={formData?.visible_until ?? ''}
-                  sites={formData?.sites ?? []}
-                />
-              </Stack>
-            </Grid>
-          </Grid>
+          <Stack
+            gap={5}
+            sx={{color: colors.black, background: colors.white, p: '10px', width: {xs: '100%', md: '50vw'}}}
+          >
+            <Post
+              id={0}
+              links={formData?.links ?? []}
+              caption={formData?.caption ?? ''}
+              short_text={formData?.short_text ?? ''}
+              details={formData?.details ?? ''}
+              added_at={formData?.added_at ?? new Date().toISOString()}
+              visible_after={formData?.visible_after ?? ''}
+              visible_until={formData?.visible_until ?? ''}
+              sites={formData?.sites ?? []}
+            />
+          </Stack>
         )}
       </FormDataConsumer>
     </ThemeProvider>
