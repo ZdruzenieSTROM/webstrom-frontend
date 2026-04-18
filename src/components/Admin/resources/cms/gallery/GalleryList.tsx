d6122eb5ceafe83475d613e684dd48d3b10944be
matushl <hlavacik.matus@gmail.com>
2026-04-18 15:00:45 +0200

    add filters by competition

--- src/components/Admin/resources/cms/gallery/GalleryList.tsx
+++ src/components/Admin/resources/cms/gallery/GalleryList.tsx
@@ -1,12 +1,21 @@
 import {FC} from 'react'
 import {Datagrid, List, TextField} from 'react-admin'
 
+import {CompetitionFilterSection} from '@/components/Admin/custom/list-filtering/CompetitionFilterSection'
+import {FilterSidebar} from '@/components/Admin/custom/list-filtering/FilterSidebar'
+
 export const GalleryList: FC = () => (
-  <List>
+  <List aside={<GalleryListFilters />}>
     <Datagrid>
       <TextField source="name" />
       <TextField source="gallery_link" />
       <TextField source="event_name" />
     </Datagrid>
   </List>
+)
+
+const GalleryListFilters: FC = () => (
+  <FilterSidebar>
+    <CompetitionFilterSection />
+  </FilterSidebar>
 )
