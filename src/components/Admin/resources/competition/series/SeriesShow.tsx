90fa7d0dcfaff1a67861292c741755dd117d05fc
Martin Mihálik <mihalik.martin@outlook.sk>
2026-04-18 19:15:39 +0200

    Fix editing of semester publications and series problems in RA

--- src/components/Admin/resources/competition/series/SeriesShow.tsx
+++ src/components/Admin/resources/competition/series/SeriesShow.tsx
@@ -3,37 +3,44 @@ import {
   ArrayField,
   BooleanField,
   Datagrid,
+  EditButton,
   ReferenceField,
   SimpleShowLayout,
   Tab,
   TabbedShowLayout,
   TextField,
+  useCreatePath,
 } from 'react-admin'
 
 import {DateTimeField} from '@/components/Admin/custom/DateTimeField'
 import {MyShow} from '@/components/Admin/custom/MyShow'
 import {TruncatedTextField} from '@/components/Admin/custom/TruncatedTextField'
 
-export const SeriesShow: FC = () => (
-  <MyShow>
-    <TabbedShowLayout>
-      <Tab label="content.labels.general">
-        <SimpleShowLayout>
-          <ReferenceField source="semester" reference="competition/semester" link="show" />
-          <DateTimeField source="deadline" />
-          <TextField source="order" />
-          <BooleanField source="complete" />
-        </SimpleShowLayout>
-      </Tab>
-      <Tab label="content.labels.problems">
-        <SimpleShowLayout>
-          <ArrayField source="problems">
-            <Datagrid rowClick={(id) => `/competition/problem/${id}/show`}>
-              <TruncatedTextField source="text" maxTextWidth={100} expandable />
-            </Datagrid>
-          </ArrayField>
-        </SimpleShowLayout>
-      </Tab>
-    </TabbedShowLayout>
-  </MyShow>
-)
+export const SeriesShow: FC = () => {
+  const createPath = useCreatePath()
+
+  return (
+    <MyShow>
+      <TabbedShowLayout>
+        <Tab label="content.labels.general">
+          <SimpleShowLayout>
+            <ReferenceField source="semester" reference="competition/semester" link="show" />
+            <DateTimeField source="deadline" />
+            <TextField source="order" />
+            <BooleanField source="complete" />
+          </SimpleShowLayout>
+        </Tab>
+        <Tab label="content.labels.problems">
+          <SimpleShowLayout>
+            <ArrayField source="problems">
+              <Datagrid rowClick={(id) => createPath({resource: 'competition/problem', type: 'show', id: id})}>
+                <TruncatedTextField source="text" maxTextWidth={100} expandable />
+                <EditButton resource="competition/problem" />
+              </Datagrid>
+            </ArrayField>
+          </SimpleShowLayout>
+        </Tab>
+      </TabbedShowLayout>
+    </MyShow>
+  )
+}
